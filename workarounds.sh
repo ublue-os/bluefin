7e1863f6760860e8a1296813cf045e0cf9b8434c
Jorge O. Castro <jorge.castro@gmail.com>
2023-06-01 14:58:30 -0400

    feat(dx): add ld workaround

---
+++ workarounds.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+set -oeux pipefail
+
+# alternatives cannot create symlinks on its own during a container build
+ln -s /usr/bin/ld.bfd /etc/alternatives/ld && ln -s /etc/alternatives/ld /usr/bin/ld
