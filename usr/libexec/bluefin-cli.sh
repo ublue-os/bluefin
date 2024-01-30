2702065639217936e67727f232971c0508ebee55
m2Giles <69128853+m2Giles@users.noreply.github.com>
2024-01-29 19:24:49 -0500

    fix: shellcheck fixes

--- usr/libexec/bluefin-cli.sh
+++ usr/libexec/bluefin-cli.sh
@@ -17,8 +17,8 @@ targets=(
 # Exit Function
 ###
 function Exiting(){
-    printf "${red}Exiting...${normal}\n"
-    printf "Rerun CLI setup using ${blue}ujust bluefin-cli${normal}...\n"
+    echo "${red}Exiting...${normal}"
+    echo "Rerun CLI setup using ${blue}ujust bluefin-cli${normal}..."
     exit 0
 }
 
@@ -28,7 +28,7 @@ function Exiting(){
 function Terminal_choice(){
     TERMINAL_CHOICE=$(Choose Host Container)
     if test "$TERMINAL_CHOICE" = "Host"; then
-        printf "You have chosen to use Host Terminal.\n"
+        echo "You have chosen to use Host Terminal."
     fi
 }
 
@@ -38,7 +38,7 @@ function Terminal_choice(){
 function Make_container(){
     MAKE_CONTAINER=0
     if test "$1" = "Host"; then
-        printf "Would you still like to setup default container?\n"
+        echo "Would you still like to setup default container?"
         MAKE_CONTAINER=$(Confirm) 
     fi
 }
@@ -49,14 +49,14 @@ function Make_container(){
 ###
 function Choose_container(){
     DX_VERSION=1
-    printf "Which Container Toolbox would you like to use?\n"
-    CONTAINER_CHOICE=$(Choose ${targets[@]})
-    if test $CONTAINER_CHOICE = "bluefin-cli" || test $CONTAINER_CHOICE = "wolfi-toolbox"; then
+    echo "Which Container Toolbox would you like to use?"
+    CONTAINER_CHOICE=$(Choose "${targets[@]}")
+    if test "$CONTAINER_CHOICE" = "bluefin-cli" || test "$CONTAINER_CHOICE" = "wolfi-toolbox"; then
         printf "Would you like to use developer toolkit version?\n"
         printf "It has packages for building Apks and other SDKs.\n"
         DX_VERSION=$(Confirm)
         if test "$DX_VERSION" -eq 0; then
-            MATCH=$(echo $CONTAINER_CHOICE | cut -d "-" -f 2)
+            MATCH=$(echo "$CONTAINER_CHOICE" | cut -d "-" -f 2)
             CONTAINER_CHOICE="${CONTAINER_CHOICE%%${MATCH}*}dx-${MATCH}${CONTAINER_CHOICE##*${MATCH}}"
         fi
     fi
@@ -67,10 +67,11 @@ function Choose_container(){
 # Ask how they want to manage the container.
 ###
 function Container_manager(){
-    printf "Would you like to use ${blue}quadlets${normal} to manage your container?\n\n"
-    printf "${blue}Quadlets automatically rebuild your container on login.${normal}\n"
-    printf "They will always be on the latest image you have pulled.\n"
-    printf "However, ${red}manually installed packages using apk, apt, or dnf will not persist${normal}.\n"
+    echo "Would you like to use ${blue}quadlets${normal} to manage your container?"
+    echo ""
+    echo "${blue}Quadlets automatically rebuild your container on login.${normal}"
+    echo "They will always be on the latest image you have pulled."
+    echo "However, ${red}manually installed packages using apk, apt, or dnf will not persist${normal}."
     CONTAINER_MANAGER=$(Choose Quadlet Distrobox)
 }
 
@@ -80,12 +81,12 @@ function Container_manager(){
 # Takes chosen container as an argument.
 ###
 function Is_enabled_and_stop(){
-    for i in ${targets[@]}
+    for i in "${targets[@]}"
     do
         Enabled=0
         Enabled=$(systemctl --user is-enabled "$i".target)
         if test "$Enabled" = "enabled" && test "$i" != "$1"; then
-            printf "$i is enabled.\n"
+            echo "$i is enabled."
             printf "Would you like to disable and stop container?\n"
             Disable=$(Confirm) 
             if test "$Disable" -eq 0; then
@@ -94,11 +95,11 @@ function Is_enabled_and_stop(){
                     systemctl --user stop "$i".service
                 fi
             else
-                printf "Not disabling and stopping existing container $i..."
+                printf "Not disabling and stopping existing container %s..." "$i"
             fi
             unset "$Disable" 
         elif test "$Enabled" = "enabled" && test "$i" = "$1"; then
-            printf "$i is already enabled${n}...\n"
+            echo "$i is already enabled..."
             Make_symlinks "$TERMINAL_CHOICE" "$1" 
             Exiting
         fi
@@ -112,19 +113,19 @@ function Is_enabled_and_stop(){
 # Takes chosen container as an argument.
 ###
 function Already_exists_and_rm(){
-    for i in ${targets[@]}
+    for i in "${targets[@]}" 
     do
         Exists=0
         Exists=$(podman ps --all --filter name="$i" | grep -q " $i\$" && echo "1" || echo "0")
         if test "$Exists" -eq 1 && test "$i" = "$1"; then
-            printf "$1 ${red}${bold}exists${normal}, would you like to ${red}${bold}delete it?${normal}\n"
+            echo "$1 ${red}${bold}exists${normal}, would you like to ${red}${bold}delete it?${normal}"
             Delete=$(Confirm)
             if test "$Delete" -eq 0; then
-                printf "Removing $1...\n"
+                echo "Removing $1..."
                 podman rm --force "$1"
             else
-                printf "Not removing $1..."
-                Exiting
+                printf "Not removing %s..." "$i"
+                ExitinG
             fi
             unset "$Delete"
         fi
@@ -141,14 +142,16 @@ function Build_container(){
         Exiting
     fi
     if test "$2" = "Quadlet"; then
-        printf "${blue}Building container using a Quadlet${normal}\n\n"
+        echo "${blue}Building container using a Quadlet${normal}"
+        echo ""
         systemctl --user enable "$3".target
         systemctl --user restart "$3".service
     elif test "$2" = "Distrobox"; then
-        printf "${blue}Building container using a Distrobox${normal}\n\n"
+        echo "${blue}Building container using a Distrobox${normal}"
+        echo ""
         distrobox-create --nvidia --no-entry -Y --image "ghcr.io/ublue-os/${3}" --name "$3"
     else
-        printf "${red}Unkown Choice${normal}..."
+        printf "Unkown Choice..."
         Exiting
     fi
 }
@@ -159,15 +162,15 @@ function Build_container(){
 ###
 function Make_symlinks(){
     if test -d "${HOME}/.bashrc.d" && test "$1" = "Host"; then
-            printf "Not making symlinks and ${red}removing existing symlink if it exists${normal}.\n"
-            test -L "~/.bashrc.d/zz-container.sh" && rm "~/.bashrc.d/00-container.sh"
+            echo "Not making symlinks and ${red}removing existing symlink if it exists${normal}."
+            test -L "${HOME}/.bashrc.d/zz-container.sh" && rm "${HOME}/.bashrc.d/00-container.sh"
     elif test -d "${HOME}/.bashrc.d"; then
-        printf "Setting first terminal be Container for bash using ~/.bashrc.d\n"
-        printf "Enter into container using prompt's menu after first entry\n"
-        printf "${blue}This requires your bash shell to source files in ~/.bashrc.d/${normal}\n"
-        ln -sf "/usr/share/ublue-os/bluefin-cli/${2}.sh" ~/.bashrc.d/00-container.sh
+        echo "Setting first terminal be Container for bash using ~/.bashrc.d"
+        echo "Enter into container using prompt's menu after first entry"
+        echo "${blue}This requires your bash shell to source files in ~/.bashrc.d/${normal}"
+        ln -sf "/usr/share/ublue-os/bluefin-cli/${2}.sh" "${HOME}/.bashrc.d/00-container.sh"
     else
-        printf "${red}Not implemented for non-Bash shells${normal} at this time...\n"
+        echo "${red}Not implemented for non-Bash shells${normal} at this time..."
     fi
 }
 
@@ -181,7 +184,8 @@ function main(){
     Already_exists_and_rm "$CONTAINER_CHOICE" 
     Build_container "$MAKE_CONTAINER" "$CONTAINER_MANAGER" "$CONTAINER_CHOICE"
     Make_symlinks "$TERMINAL_CHOICE" "$CONTAINER_CHOICE"
-    printf "Finished Bluefin-CLI setup, rerun with ${blue}ujust bluefin-cli${normal} to reconfigure\n"
+    echo ""
+    echo "Finished Bluefin-CLI setup, rerun with ${blue}ujust bluefin-cli${normal} to reconfigure"
 }
 
 main
