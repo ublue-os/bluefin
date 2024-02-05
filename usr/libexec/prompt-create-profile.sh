663742dd32c9e1d8f99576a08bbbf243f78c041a
m2Giles <69128853+m2Giles@users.noreply.github.com>
2024-02-04 21:19:17 -0500

    feat: give bluefin-cli a cattppuccin-dynamic colorshcme

--- usr/libexec/prompt-create-profile.sh
+++ usr/libexec/prompt-create-profile.sh
@@ -26,12 +26,17 @@ gen_uuid() {
 
 guid=$(gen_uuid)
 name="$1"
+palette="$2"
 
 profile="/org/gnome/Prompt/Profiles/${guid}/"
 
 dconf write "${profile}custom-command" "'sh -c \"[ ! -e /run/.containerenv ] && [ ! -e /run/.dockerenv ] && distrobox enter ${name} || ${SHELL}\"'"
 dconf write "${profile}label" "'${name}'"
-dconf write "${profile}login-shell" "true"
 dconf write "${profile}use-custom-command" "true"
+if test -n "$palette"; then
+	dconf write "${profile}palette" "'${palette}'"
+elif test "$name" = "bluefin-cli"; then
+	dconf write "${profile}palette" "'catppuccin-dynamic'"
+fi
 
 /usr/libexec/prompt-add-profile.sh "$guid"
