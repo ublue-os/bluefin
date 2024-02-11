267e5be35d824a5c921f18bce9fc151ed6762947
m2 <69128853+m2Giles@users.noreply.github.com>
2024-02-11 11:57:57 -0800

    feat: improve prompt integration with quadlets (#889)

--- usr/libexec/prompt-create-profile.sh
+++ usr/libexec/prompt-create-profile.sh
@@ -15,8 +15,11 @@
 
 # if dconf doesn't exist, just return
 if ! command -v dconf >/dev/null; then
-    return
+    exit 0
 fi
+
+# shellcheck disable=SC1091
+. /usr/share/ublue-os/bluefin-cli/known-containers
 
 # shellcheck disable=SC2001
 gen_uuid() {
@@ -24,19 +27,63 @@ gen_uuid() {
 	echo "$uuid" | sed 's/-//g'
 }
 
-guid=$(gen_uuid)
 name="$1"
-palette="$2"
+default="$2"
+palette="$3"
+
+for check in "${!known_container[@]}"; do
+	if test "$check" = "$name"; then
+		guid=${known_container[$check]}
+	fi
+done
+
+if test -z "$guid"; then
+	guid=$(gen_uuid)
+fi
+
+default_guid=$(dconf read /org/gnome/Prompt/default-profile-uuid)
+default_guid=${default_guid:1:-1}
+
+# If default profile is trying to be made, just exit
+if test "$guid" = "$default_guid"; then
+	exit 0	
+fi
+
+if test -z "$default"; then
+    make_default=0
+elif test "$default" = "default" || test "$default" -eq 1; then
+    make_default=1
+fi
+
+# Write the default value if specified
+if test "$make_default" -eq 1; then
+    dconf write /org/gnome/Prompt/default-profile-uuid "'${guid}'"
+fi
 
 profile="/org/gnome/Prompt/Profiles/${guid}/"
+opacity=$(dconf read /org/gnome/Prompt/Profiles/"${default_guid}"/opacity)
 
-dconf write "${profile}custom-command" "'sh -c \"[ ! -e /run/.containerenv ] && [ ! -e /run/.dockerenv ] && distrobox enter ${name} || ${SHELL}\"'"
-dconf write "${profile}label" "'${name}'"
-dconf write "${profile}use-custom-command" "true"
+if test "$name" = "Host"; then
+	dconf write "${profile}label" "'${name}'"
+else
+	dconf write "${profile}custom-command" "'sh -c \"[ ! -e /run/.containerenv ] && exec distrobox enter ${name} || ${SHELL}\"'"
+	dconf write "${profile}label" "'${name}'"
+	dconf write "${profile}use-custom-command" "true"
+	dconf write "${profile}ublue-os" "true"
+fi
+
+if test -n "$opacity"; then
+	dconf write "${profile}opacity" "'${opacity}'"
+fi
+
 if test -n "$palette"; then
 	dconf write "${profile}palette" "'${palette}'"
-elif test "$name" = "bluefin-cli"; then
+elif test "$name" = "bluefin-cli" || test "$name" = "bluefin-dx-cli"; then
 	dconf write "${profile}palette" "'catppuccin-dynamic'"
+elif test "$name" = "fedora-toolbox"; then
+	dconf write "${profile}palette" "'Elio'"
+elif test "$name" = "ubuntu-toolbox"; then
+	dconf write "${profile}palette" "'Clone Of Ubuntu'"
 fi
 
 /usr/libexec/prompt-add-profile.sh "$guid"
