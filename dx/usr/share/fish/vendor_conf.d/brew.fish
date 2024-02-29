601251fe697034e3222a5999883c4fffef456b29
Dylan M. Taylor <dylan@dylanmtaylor.com>
2024-02-28 23:27:44 -0500

    Make this check if brew is installed before trying to run it.

--- dx/usr/share/fish/vendor_conf.d/brew.fish
+++ dx/usr/share/fish/vendor_conf.d/brew.fish
@@ -1,1 +1,1 @@
-status --is-interactive && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+command -v brew >/dev/null 2>&1 && status --is-interactive && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
