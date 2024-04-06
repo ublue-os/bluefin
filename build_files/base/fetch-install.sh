4da56d25b8fbeebdc53f4c0251c3e946c99bf9ec
m2Giles <69128853+m2Giles@users.noreply.github.com>
2024-04-06 18:24:15 -0400

    chore: remove unnecessary && and \

--- build_files/base/fetch-install.sh
+++ build_files/base/fetch-install.sh
@@ -3,17 +3,17 @@
 set -oue pipefail
 
 # Starship Shell Prompt
-curl -Lo /tmp/starship.tar.gz "https://github.com/starship/starship/releases/latest/download/starship-x86_64-unknown-linux-gnu.tar.gz" && \
-tar -xzf /tmp/starship.tar.gz -C /tmp && \
-install -c -m 0755 /tmp/starship /usr/bin && \
+curl -Lo /tmp/starship.tar.gz "https://github.com/starship/starship/releases/latest/download/starship-x86_64-unknown-linux-gnu.tar.gz"
+tar -xzf /tmp/starship.tar.gz -C /tmp
+install -c -m 0755 /tmp/starship /usr/bin
 echo 'eval "$(starship init bash)"' >> /etc/bashrc
 
 # Brew Install Script
-wget https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh -O /usr/libexec/brew-install && \
-    chmod +x /usr/libexec/brew-install
+wget https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh -O /usr/libexec/brew-install
+chmod +x /usr/libexec/brew-install
 
 # Flatpak Remotes
-mkdir -p /usr/etc/flatpak/remotes.d && \
+mkdir -p /usr/etc/flatpak/remotes.d
 wget -q https://dl.flathub.org/repo/flathub.flatpakrepo -P /usr/etc/flatpak/remotes.d
 
 # Topgrade Install
--- build_files/dx/fetch-install-dx.sh
+++ build_files/dx/fetch-install-dx.sh
@@ -2,6 +2,6 @@
 
 set -oue pipefail
 
-curl -Lo ./kind "https://github.com/kubernetes-sigs/kind/releases/latest/download/kind-$(uname)-amd64" && \
-    chmod +x ./kind && \
-    mv ./kind /usr/bin/kind
+curl -Lo ./kind "https://github.com/kubernetes-sigs/kind/releases/latest/download/kind-$(uname)-amd64"
+chmod +x ./kind
+mv ./kind /usr/bin/kind
