30eb33746c7e8249db1c54f5cebfb121fc371679
m2 <69128853+m2Giles@users.noreply.github.com>
2024-04-11 18:58:34 -0400

    chore: combine Aurora and Bluefin yafti (#1119)
    Co-authored-by: Jorge O. Castro <jorge.castro@gmail.com>

--- build_files/dx/branding-dx.sh
+++ build_files/dx/branding-dx.sh
@@ -4,6 +4,8 @@ set -oue pipefail
 
 if test "$BASE_IMAGE_NAME" = "silverblue"; then
     sed -i '/^PRETTY_NAME/s/Bluefin/Bluefin-dx/' /usr/lib/os-release
+    sed -i 's/Bluefin/Bluefin-dx/' /usr/etc/yafti.yml
 elif test "$BASE_IMAGE_NAME" = "kinoite"; then
     sed -i '/^PRETTY_NAME/s/Aurora/Aurora-dx/' /usr/lib/os-release
+    sed -i 's/Aurora/Aurora-dx/' /usr/etc/yafti.yml
 fi
