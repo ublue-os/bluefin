7ce24299e87197e2e2cb5cf3f2dd27e624231255
m2Giles <69128853+m2Giles@users.noreply.github.com>
2024-04-11 11:07:12 -0400

    fix: yafti file path

--- build_files/dx/branding-dx.sh
+++ build_files/dx/branding-dx.sh
@@ -4,7 +4,7 @@ set -oue pipefail
 
 if test "$BASE_IMAGE_NAME" = "silverblue"; then
     sed -i '/^PRETTY_NAME/s/Bluefin/Bluefin-dx/' /usr/lib/os-release
-    sed -i 's/Bluefin/Bluefin-dx/' /tmp/yafti.yml
+    sed -i 's/Bluefin/Bluefin-dx/' /usr/etc/yafti.yml
 elif test "$BASE_IMAGE_NAME" = "kinoite"; then
     sed -i '/^PRETTY_NAME/s/Aurora/Aurora-dx/' /usr/lib/os-release
     sed -i 's/Aurora/Aurora-dx/' /usr/etc/yafti.yml
