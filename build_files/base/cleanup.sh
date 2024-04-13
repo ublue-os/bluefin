56b58ab5c5a93af339b1579c69d21cb564b740d7
m2 <69128853+m2Giles@users.noreply.github.com>
2024-04-13 13:46:47 -0400

    chore: branding and cleaning (#1128)

--- build_files/base/cleanup.sh
+++ build_files/base/cleanup.sh
@@ -3,17 +3,16 @@
 set -ouex pipefail
 
 if [[ "${IMAGE_FLAVOR}" =~ "nvidia" ]]; then
-  rm /usr/etc/dracut.conf.d/nvidia.conf
-  rm /usr/lib/modprobe.d/nvidia.conf
+    rm /usr/lib/modprobe.d/nvidia.conf
 fi
 
 rm -f /etc/yum.repos.d/tailscale.repo
 rm -f /etc/yum.repos.d/charm.repo
 rm -f /etc/yum.repos.d/ublue-os-staging-fedora-"${FEDORA_MAJOR_VERSION}".repo
-echo "Hidden=true" >> /usr/share/applications/fish.desktop
-echo "Hidden=true" >> /usr/share/applications/htop.desktop
-echo "Hidden=true" >> /usr/share/applications/nvtop.desktop
-if [ "$BASE_IMAGE_NAME" = "silverblue" ]; then
-    echo "Hidden=true" >> /usr/share/applications/gnome-system-monitor.desktop
+sed -i 's@\[Desktop Entry\]@\[Desktop Entry\]\nHidden=true@g' /usr/share/applications/fish.desktop
+sed -i 's@\[Desktop Entry\]@\[Desktop Entry\]\nHidden=true@g' /usr/share/applications/htop.desktop
+sed -i 's@\[Desktop Entry\]@\[Desktop Entry\]\nHidden=true@g' /usr/share/applications/nvtop.desktop
+if [[ "$BASE_IMAGE_NAME" = "silverblue" && -f /usr/share/applications/gnome-system-monitor.desktop ]]; then
+    sed -i 's@\[Desktop Entry\]@\[Desktop Entry\]\nHidden=true@g' /usr/share/applications/gnome-system-monitor.desktop
 fi
 rm -f /etc/yum.repos.d/_copr_che-nerd-fonts-"${FEDORA_MAJOR_VERSION}".repo
