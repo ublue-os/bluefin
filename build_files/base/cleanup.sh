3c25fd98b376b279fc943f7a401007335395e659
m2Giles <69128853+m2Giles@users.noreply.github.com>
2024-04-13 13:15:11 -0400

    fix: gnome-system-monitor name is different in f40

--- build_files/base/cleanup.sh
+++ build_files/base/cleanup.sh
@@ -12,7 +12,7 @@ rm -f /etc/yum.repos.d/ublue-os-staging-fedora-"${FEDORA_MAJOR_VERSION}".repo
 sed -i 's@\[Desktop Entry\]@\[Desktop Entry\]\nHidden=true@g' /usr/share/applications/fish.desktop
 sed -i 's@\[Desktop Entry\]@\[Desktop Entry\]\nHidden=true@g' /usr/share/applications/htop.desktop
 sed -i 's@\[Desktop Entry\]@\[Desktop Entry\]\nHidden=true@g' /usr/share/applications/nvtop.desktop
-if [ "$BASE_IMAGE_NAME" = "silverblue" ]; then
+if [[ "$BASE_IMAGE_NAME" = "silverblue" && -f /usr/share/applications/gnome-system-monitor.desktop ]]; then
     sed -i 's@\[Desktop Entry\]@\[Desktop Entry\]\nHidden=true@g' /usr/share/applications/gnome-system-monitor.desktop
 fi
 rm -f /etc/yum.repos.d/_copr_che-nerd-fonts-"${FEDORA_MAJOR_VERSION}".repo
