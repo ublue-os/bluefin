4bbe588a783366432035f5bfdfe565f93a14f409
m2Giles <69128853+m2Giles@users.noreply.github.com>
2024-04-12 20:17:38 -0400

    chore: branded watermark

--- build_files/base/branding.sh
+++ build_files/base/branding.sh
@@ -4,6 +4,7 @@ set -oue pipefail
 
 if test "$BASE_IMAGE_NAME" = "silverblue"; then
     sed -i '/^PRETTY_NAME/s/Silverblue/Bluefin/' /usr/lib/os-release
+    cp /usr/share/plymouth/themes/spinner/{silverblue-,}watermark.png
 elif test "$BASE_IMAGE_NAME" = "kinoite"; then
     sed -i '/^PRETTY_NAME/s/Kinoite/Aurora/' /usr/lib/os-release
     sed -i 's/Bluefin/Aurora/g' /usr/etc/yafti.yml
