57fcbd361928547689fdadf8f1f025e65ed75f85
Robert Sturla <robertsturla@outlook.com>
2024-04-18 19:08:18 +0100

    fix: source systemd-dx.sh from build-dx.sh script (#1155)

--- build_files/dx/build-dx.sh
+++ build_files/dx/build-dx.sh
@@ -11,5 +11,6 @@ sysctl -p
 . /tmp/build/image-info.sh
 . /tmp/build/fetch-install-dx.sh
 . /tmp/build/workarounds.sh
+. /tmp/build/systemd-dx.sh
 . /tmp/build/branding-dx.sh
 . /tmp/build/cleanup-dx.sh
