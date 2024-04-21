7f00c93c278d1061153d65f5fbca32c788f0c9a1
m2Giles <69128853+m2Giles@users.noreply.github.com>
2024-04-21 17:33:34 -0400

    fix: jq type and flashing lock

--- system_files/shared/usr/libexec/ublue-image-info.sh
+++ system_files/shared/usr/libexec/ublue-image-info.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/bash
 
-echo -n "$(jq -r '\"\\(.[\"image-name\"]):\\(.[\"image-tag\"])\"'  < /usr/share/ublue-os/image-info.json)"
+echo -n $(jq -r '"\(.["image-name"]):\(.["image-tag"])"' < /usr/share/ublue-os/image-info.json)
 
 if [[ $(rpm-ostree status --booted) =~ "signed" ]]; then
 	echo -n "🔐"
 else
-	echo -n "🔒"
+	echo -n -e "\033[5m🔒"
 fi
