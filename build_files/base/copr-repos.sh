102ff6983e3eb16ef91da4f778304cb2a51863c5
m2 <69128853+m2Giles@users.noreply.github.com>
2024-04-22 10:09:18 -0400

    chore: cleanup shellcheck warnings (#1181)

--- build_files/base/copr-repos.sh
+++ build_files/base/copr-repos.sh
@@ -6,7 +6,7 @@ set -oue pipefail
 wget https://copr.fedorainfracloud.org/coprs/ublue-os/staging/repo/fedora-"${FEDORA_MAJOR_VERSION}"/ublue-os-staging-fedora-"${FEDORA_MAJOR_VERSION}".repo -O /etc/yum.repos.d/ublue-os-staging-fedora-"${FEDORA_MAJOR_VERSION}".repo
 
 # 39 gets VRR and Ptyxis
-if [ ${FEDORA_MAJOR_VERSION} -eq "39" ]; then
+if [ "${FEDORA_MAJOR_VERSION}" -eq "39" ]; then
     wget https://copr.fedorainfracloud.org/coprs/kylegospo/gnome-vrr/repo/fedora-"${FEDORA_MAJOR_VERSION}"/kylegospo-gnome-vrr-fedora-"${FEDORA_MAJOR_VERSION}".repo -O /etc/yum.repos.d/_copr_kylegospo-gnome-vrr.repo
     rpm-ostree override replace --experimental --from repo=copr:copr.fedorainfracloud.org:kylegospo:gnome-vrr mutter mutter-common gnome-control-center gnome-control-center-filesystem
     rm -f /etc/yum.repos.d/_copr_kylegospo-gnome-vrr.repo
@@ -21,7 +21,7 @@ if [ ${FEDORA_MAJOR_VERSION} -eq "39" ]; then
 fi
 
 # 40 gets Ptyxis and patched Mutter
-if [ ${FEDORA_MAJOR_VERSION} -eq "40" ]; then
+if [ "${FEDORA_MAJOR_VERSION}" -eq "40" ]; then
     rpm-ostree override replace \
     --experimental \
     --from repo=copr:copr.fedorainfracloud.org:ublue-os:staging \
