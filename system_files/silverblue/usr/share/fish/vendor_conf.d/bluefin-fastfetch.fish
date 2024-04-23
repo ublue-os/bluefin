c37f09a82319dba92d6191ba5d8aabe0de463125
m2Giles <69128853+m2Giles@users.noreply.github.com>
2024-04-23 16:31:28 -0400

    chore: add fish alias for bluefin fastfetch

---
+++ system_files/silverblue/usr/share/fish/vendor_conf.d/bluefin-fastfetch.fish
@@ -0,0 +1,2 @@
+alias fastfetch="/usr/bin/fastfetch --logo /usr/share/ublue-os/bluefin-logo -c /usr/share/ublue-os/ublue-os.jsonc"
+alias neofetch="/usr/bin/fastfetch --logo /usr/share/ublue-os/bluefin-logo -c /usr/share/ublue-os/ublue-os.jsonc"
