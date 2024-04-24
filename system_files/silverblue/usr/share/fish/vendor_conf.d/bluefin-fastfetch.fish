0971f41b0c26c5c358e91c8c1bcf37d961aa7e36
m2Giles <69128853+m2Giles@users.noreply.github.com>
2024-04-24 15:19:43 -0400

    chore: remove neofetch alias

--- system_files/silverblue/usr/share/fish/vendor_conf.d/bluefin-fastfetch.fish
+++ system_files/silverblue/usr/share/fish/vendor_conf.d/bluefin-fastfetch.fish
@@ -1,2 +1,1 @@
-alias fastfetch="/usr/bin/fastfetch --logo /usr/share/ublue-os/bluefin-logo -c /usr/share/ublue-os/ublue-os.jsonc"
-alias neofetch="/usr/bin/fastfetch --logo /usr/share/ublue-os/bluefin-logo -c /usr/share/ublue-os/ublue-os.jsonc"
+alias fastfetch="/usr/bin/fastfetch --logo /usr/share/ublue-os/bluefin-logos/symbols/dolly -c /usr/share/ublue-os/ublue-os.jsonc"
