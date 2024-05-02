b6db5e3a8c2b3fab41de298b994d7f6285cae90a
m2Giles <69128853+m2Giles@users.noreply.github.com>
2024-05-02 21:57:08 +0000

    fix: remove duplicate remove

--- build_files/dx/cleanup-dx.sh
+++ build_files/dx/cleanup-dx.sh
@@ -13,9 +13,6 @@ sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/_copr:copr.fedorainfracloud.or
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/fedora-cisco-openh264.repo
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/_copr_ublue-os-akmods.repo
 
-if [[ ${FEDORA_MAJOR_VERSION} -eq "39" ]]; then
-    sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/_copr_kylegospo-gnome-vrr.repo
-fi
 for i in /etc/yum.repos.d/rpmfusion-*; do
     sed -i 's@enabled=1@enabled=0@g' "$i"
 done
