f69fe8084a45590ccc3332f1c8705853660bb874
m2Giles <69128853+m2Giles@users.noreply.github.com>
2024-05-03 17:59:57 +0000

    fix: add all three grub states.

--- system_files/shared/usr/libexec/configure-grub.sh
+++ system_files/shared/usr/libexec/configure-grub.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
 source /usr/lib/ujust/ujust.sh
 GRUB_STATE="$(sudo grub2-editenv list | grep "^menu_auto_hide=")"
-if [ "$GRUB_STATE" == "menu_auto_hide=0" ] ; then
-    GRUB_STATE="${b}Not Hidden${n}"
+if [[ "$GRUB_STATE" == "menu_auto_hide=1" ]]; then
+    GRUB_STATE="${bold}Hidden${normal}"
+elif [[ "$GRUB_STATE" == "menu_auto_hide=2" ]]; then
+    GRUB_STATE="${bold}Always Hidden${normal}"
 else
-    GRUB_STATE="${b}Hidden${n}"
+    GRUB_STATE="${bold}Unhidden${normal}"
 fi
 echo "${bold}Grub menu configuration${normal}"
 echo "Grub menu is set to: $GRUB_STATE"
-OPTION=$(Choose "Hide Grub" "Unhide Grub" )
-if [[ "${OPTION,,}" =~ ^hide ]]; then
-    sudo grub2-editenv - set menu_auto_hide=2 boot_success=1
-    elif [[ "${OPTION,,}" =~ ^unhide ]]; then
-    sudo grub2-editenv - set menu_auto_hide=0 boot_success=1
+OPTION=$(Choose "Always Hide Grub" "Hide Grub" "Show Grub" "Cancel")
+if [[ "${OPTION,,}" =~ ^always ]]; then
+    sudo grub2-editenv - set menu_auto_hide=2
+    GRUB_STATE="${bold}Always Hidden${normal}"
+elif [[ "${OPTION,,}" =~ ^hide ]]; then
+    sudo grub2-editenv - set menu_auto_hide=1
+    GRUB_STATE="${bold}Hidden${normal}"
+elif [[ "${OPTION,,}" =~ ^show ]]; then
+    sudo grub2-editenv - set menu_auto_hide=0
+    GRUB_STATE="${bold}Unhidden${normal}"
+else
+    echo "Not Changing Settings"
 fi
+echo "Grub menu is set to: $GRUB_STATE"
