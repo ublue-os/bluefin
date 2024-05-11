747805b6ae2b3d2f89d267df1159865fb47b8cea
m2 <69128853+m2Giles@users.noreply.github.com>
2024-05-11 18:35:39 -0400

    fix: brew installer

--- build_files/base/brew.sh
+++ build_files/base/brew.sh
@@ -2,5 +2,6 @@
 
 curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh > /tmp/brew.sh
 chmod +x /tmp/brew.sh
+rm -f /home
 env CLI=1 NONINTERACTIVE=1 /tmp/brew.sh
 cp -R /home/linuxbrew/.linuxbrew /usr/lib/homebrew
