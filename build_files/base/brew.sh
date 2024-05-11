a044d8e6f27637ed6f17f0908260abb5c972a4bc
m2 <69128853+m2Giles@users.noreply.github.com>
2024-05-11 18:03:51 -0400

    fix: link to var/home

--- build_files/base/brew.sh
+++ build_files/base/brew.sh
@@ -2,6 +2,5 @@
 
 curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh > /tmp/brew.sh
 chmod +x /tmp/brew.sh
-mkdir /var/home
-ln -s /usr/lib/homebrew /var/home/linuxbrew
+ln -s /usr/lib/homebrew /var/home
 env CLI=1 NONINTERACTIVE=1 /tmp/brew.sh 
