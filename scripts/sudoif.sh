53c1e1ebfd4ae82cc3dcbf360fd827845da336c9
m2Giles <69128853+m2Giles@users.noreply.github.com>
2024-05-14 00:30:20 +0000

    fix(dev): logical and not or

--- scripts/sudoif.sh
+++ scripts/sudoif.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 function sudoif(){
     if [[ "${TERM_PROGRAM}" == "vscode" ]]; then
-        if [[ ! -f /run/.containerenv || ! -f /.dockerenv ]]; then
+        if [[ ! -f /run/.containerenv && ! -f /.dockerenv ]]; then
             /usr/bin/systemd-run --uid=0 --gid=0 -d -E TERM="$TERM" -t -q -P -G "$@"
         else
             /usr/bin/sudo "$@"
