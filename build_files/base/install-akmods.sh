8c33966409e740dbefe377ad5b56c9347ddeced5
m2 <69128853+m2Giles@users.noreply.github.com>
2024-06-25 17:26:11 -0400

    fix: correct akmods path

--- build_files/base/install-akmods.sh
+++ build_files/base/install-akmods.sh
@@ -21,8 +21,8 @@ if [[ "${AKMODS_FLAVOR}" =~ "coreos" ]]; then
     curl -Lo /etc/yum.repos.d/ublue-os-ucore-fedora.repo \
         https://copr.fedorainfracloud.org/coprs/ublue-os/ucore/repo/fedora/ublue-os-ucore-fedora.repo
     KERNEL_FOR_DEPMOD="$(rpm -q kernel --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
-    rpm-ostree install /tmp/coreos/rpms/*.rpm \
-                       /tmp/coreos/rpms/zfs/*rpm \
+    rpm-ostree install /tmp/coreos/akmods-rpms/*.rpm \
+                       /tmp/coreos/akmods-rpms/zfs/*.rpm \
                        pv
     depmod -A "${KERNEL_FOR_DEPMOD}"
     sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/ublue-os-ucore-fedora.repo
