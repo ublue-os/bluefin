3158ba2c3764779feb46ab6d95ad667d6dd1ab92
m2 <69128853+m2Giles@users.noreply.github.com>
2024-06-28 16:19:41 -0400

    fix: reorder nvidia install for coreos (#1459)

--- build_files/base/cleanup.sh
+++ build_files/base/cleanup.sh
@@ -19,3 +19,7 @@ sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/fedora-cisco-openh264.repo
 for i in /etc/yum.repos.d/rpmfusion-*; do
     sed -i 's@enabled=1@enabled=0@g' "$i"
 done
+
+if [ -f /etc/yum.repos.d/fedora-coreos-pool.repo ]; then
+    sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/fedora-coreos-pool.repo
+fi
