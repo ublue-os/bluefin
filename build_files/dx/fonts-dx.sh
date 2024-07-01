5925b21d44b0ca650212bfd5cf4dcad182f8f02c
Robert Sturla <robertsturla@outlook.com>
2024-07-01 21:55:03 +0100

    Prevent globbing and word splitting, whatever that is

--- build_files/dx/fonts-dx.sh
+++ build_files/dx/fonts-dx.sh
@@ -4,7 +4,7 @@ set -ouex pipefail
 
 # GitHub Monaspace
 DOWNLOAD_URL=$(curl https://api.github.com/repos/githubnext/monaspace/releases/latest | jq -r '.assets[] | select(.name| test(".*.zip$")).browser_download_url')
-curl -Lo /tmp/monaspace-font.zip $DOWNLOAD_URL
+curl -Lo /tmp/monaspace-font.zip "$DOWNLOAD_URL"
 
 unzip -qo /tmp/monaspace-font.zip -d /tmp/monaspace-font
 mkdir -p /usr/share/fonts/monaspace
