94a38ed060fd4776447c8b7aa193eb7fca167cf9
Jorge O. Castro <jorge.castro@gmail.com>
2024-07-08 10:43:42 -0400

    fix: update gnome-vrr copr (#1492)

--- build_files/base/copr-repos.sh
+++ build_files/base/copr-repos.sh
@@ -10,15 +10,16 @@ curl -Lo /etc/yum.repos.d/ublue-os-bling-fedora-"${FEDORA_MAJOR_VERSION}".repo h
 
 # 39 Ptyxis
 if [ "${FEDORA_MAJOR_VERSION}" -eq "39" ]; then
-    curl -Lo /etc/yum.repos.d/_copr_kylegospo-gnome-vrr.repo https://copr.fedorainfracloud.org/coprs/kylegospo/gnome-vrr/repo/fedora-"${FEDORA_MAJOR_VERSION}"/kylegospo-gnome-vrr-fedora-"${FEDORA_MAJOR_VERSION}".repo
-    rpm-ostree override replace --experimental --from repo=copr:copr.fedorainfracloud.org:kylegospo:gnome-vrr mutter mutter-common gnome-control-center gnome-control-center-filesystem
-    rm -f /etc/yum.repos.d/_copr_kylegospo-gnome-vrr.repo
     rpm-ostree override replace \
     --experimental \
     --from repo=copr:copr.fedorainfracloud.org:ublue-os:staging \
         gtk4 \
         vte291 \
-        libadwaita
+        libadwaita \
+        mutter \
+        mutter-common \
+        gnome-control-center \
+        gnome-control-center-filesystem 
     rpm-ostree install ptyxis
 fi
 
