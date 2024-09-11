6517ab35893fed33ace94318970c685079d6a4dc
Jarred Wilson <jarred.wilson12@gmail.com>
2024-09-11 21:27:19 +0000

    Remove fq paths to some bins

--- system_files/shared/usr/libexec/sb-key-notify.sh
+++ system_files/shared/usr/libexec/sb-key-notify.sh
@@ -11,10 +11,9 @@ TIP_PATH="/usr/share/ublue-os/motd/tips/key-warning.md"
 mokutil --test-key /etc/pki/akmods/certs/akmods-ublue.der
 
 if [ $? -ne 1 ]; then
-    USER_ID="$(/usr/bin/loginctl list-users --output=json | jq -r '.[] | .user')"
-    XDG_DIR="$(/usr/bin/loginctl show-user $USER_ID | grep RuntimePath | cut -c 13-)"
-    /usr/bin/sudo -u \
-        "$USER_ID" \
+    USER_ID="$(loginctl list-users --output=json | jq -r '.[] | .user')"
+    XDG_DIR="$(loginctl show-user $USER_ID | grep RuntimePath | cut -c 13-)"
+    sudo -u "$USER_ID" \
         "DISPLAY=:0" \
         "DBUS_SESSION_BUS_ADDRESS=unix:path=$XDG_DIR/bus" \
         notify-send \
