5b9374328d87fffc3afa5c2b3006b3f1a5e5503a
Jarred Wilson <jarred.wilson12@gmail.com>
2024-09-12 11:43:18 +0000

    Move motd warning outside of tips

--- system_files/shared/usr/libexec/sb-key-notify.sh
+++ system_files/shared/usr/libexec/sb-key-notify.sh
@@ -6,7 +6,7 @@ if [ "$(id -u)" -ne 0 ]; then
 fi
 
 WARNING_MSG="This machine has secure boot turned on, but you haven't enrolled Universal Blue's keys. Failing to enroll these before rebooting **may cause your system to fail to boot**. Follow this link https://docs.projectbluefin.io/introduction#secure-boot ~for instructions on how to enroll the keys."
-TIP_PATH="/usr/share/ublue-os/motd/tips/key-warning.md"
+KEY_WARN_FILE="/usr/share/ublue-os/motd/key-warning.md"
 
 mokutil --sb-state | grep -q enabled
 SB_ENABLED=$?
@@ -27,7 +27,7 @@ if [ $? -ne 1 ] && [ $SB_ENABLED -eq 0 ]; then
         -a mokutil \
         --wait
 
-    echo "WARNING: $WARNING_MSG" > $TIP_PATH
+    echo "WARNING: $WARNING_MSG" > $KEY_WARN_FILE
 else
-    [ -e $TIP_PATH ] && rm $TIP_PATH
+    [ -e $KEY_WARN_FILE ] && rm $KEY_WARN_FILE
 fi
