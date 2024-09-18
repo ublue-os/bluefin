dbc8a6581bb152061a6a887539ef0f98665e2258
Jarred Wilson <jarred.wilson12@gmail.com>
2024-09-18 13:13:10 +0000

    Fix linting and update script perm

--- system_files/shared/etc/profile.d/sb-key-notify-autostart.sh
+++ system_files/shared/etc/profile.d/sb-key-notify-autostart.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/bash
+
 if test "$(id -u)" -gt "0" && test -d "$HOME"; then
     if test ! -e "$HOME"/.config/autostart/sb-key-notify.desktop; then
         mkdir -p "$HOME"/.config/autostart
