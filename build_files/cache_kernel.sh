fa64a0b3a629941f6e8da9d23d21ca612c27950e
Benjamin Sherman <benjamin@holyarmy.org>
2024-10-06 18:00:14 -0500

    chore: attempt to fix rechunker complaint of multiple kernels
    PR #1722 passed all build checks, but after merging, we saw that for
    latest, where we use the stock upstream kernel, but with our added
    signature, rechunker complains of multiple kernels in /usr/lib/modules
    
    Also, the method which was being used to remove the old kernel packages
    does not run when building locally on a Fedora host. So trying a
    different approach.
    
    See: https://github.com/ublue-os/bluefin/actions/runs/11205608165/job/31145333321#step:23:233

--- build_files/cache_kernel.sh
+++ build_files/cache_kernel.sh
@@ -3,8 +3,10 @@
 set -eoux pipefail
 
 if [[ -n "${NVIDIA_TYPE:-}" ]]; then
-    rpm-ostree override remove \
-        kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra
+    for pkg in kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra
+    do
+        rpm --erase $pkg --nodeps
+    done
 
     rpm-ostree install \
         /tmp/kernel-rpms/kernel-[0-9]*.rpm \
