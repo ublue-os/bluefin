9be79ad72c3590aa83dd28f4921ff0ef9cc2168d
m2Giles <69128853+m2Giles@users.noreply.github.com>
2024-10-07 12:48:56 -0400

    fix: fix os-release naming
    Signed-off-by: m2Giles <69128853+m2Giles@users.noreply.github.com>

--- build_files/branding.sh
+++ build_files/branding.sh
@@ -3,10 +3,7 @@
 set -ouex pipefail
 
 # Branding for Bluefin/Aurora
-if test "$BASE_IMAGE_NAME" = "silverblue"; then
-    sed -i '/^PRETTY_NAME/s/Silverblue/Bluefin/' /usr/lib/os-release
-elif test "$BASE_IMAGE_NAME" = "kinoite"; then
-    sed -i '/^PRETTY_NAME/s/Kinoite/Aurora/' /usr/lib/os-release
+if test "$BASE_IMAGE_NAME" = "kinoite"; then
     sed -i 's/Bluefin/Aurora/g' /etc/yafti.yml
     sed -i 's/Aurora (Beta)/Aurora \- Bluefin\-KDE (Alpha)/' /etc/yafti.yml
     sed -i 's/Bluefin/Aurora/' /usr/libexec/ublue-flatpak-manager
