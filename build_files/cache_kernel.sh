39e90ed875332a67fff806123a57695ac4e9d363
Benjamin Sherman <benjamin@holyarmy.org>
2024-10-07 00:33:21 -0500

    chore(ci): replace vanilla kernels not asus/surface (#1754)

--- build_files/cache_kernel.sh
+++ build_files/cache_kernel.sh
@@ -2,12 +2,14 @@
 
 set -eoux pipefail
 
-for pkg in kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra
-do
-    rpm --erase $pkg --nodeps
-done
+if [[ "${AKMODS_FLAVOR}" == "main" || "${AKMODS_FLAVOR}" =~ "coreos-" ]]; then
+    for pkg in kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra
+    do
+        rpm --erase $pkg --nodeps
+    done
 
-rpm-ostree install \
-    /tmp/kernel-rpms/kernel-[0-9]*.rpm \
-    /tmp/kernel-rpms/kernel-core-*.rpm \
-    /tmp/kernel-rpms/kernel-modules-*.rpm
+    rpm-ostree install \
+        /tmp/kernel-rpms/kernel-[0-9]*.rpm \
+        /tmp/kernel-rpms/kernel-core-*.rpm \
+        /tmp/kernel-rpms/kernel-modules-*.rpm
+fi
