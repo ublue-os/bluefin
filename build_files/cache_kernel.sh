0b40fe2f5955dee33fecc03bb26412f0b11d7be8
Benjamin Sherman <benjamin@holyarmy.org>
2024-10-06 22:30:22 -0500

    chore(ci): always replace kernel packages (#1752)

--- build_files/cache_kernel.sh
+++ build_files/cache_kernel.sh
@@ -2,14 +2,12 @@
 
 set -eoux pipefail
 
-if [[ -n "${NVIDIA_TYPE:-}" ]]; then
-    for pkg in kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra
-    do
-        rpm --erase $pkg --nodeps
-    done
+for pkg in kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra
+do
+    rpm --erase $pkg --nodeps
+done
 
-    rpm-ostree install \
-        /tmp/kernel-rpms/kernel-[0-9]*.rpm \
-        /tmp/kernel-rpms/kernel-core-*.rpm \
-        /tmp/kernel-rpms/kernel-modules-*.rpm
-fi
+rpm-ostree install \
+    /tmp/kernel-rpms/kernel-[0-9]*.rpm \
+    /tmp/kernel-rpms/kernel-core-*.rpm \
+    /tmp/kernel-rpms/kernel-modules-*.rpm
