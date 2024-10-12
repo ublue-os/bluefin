d869896c9c106217b8afff86afdc09b9fd7ac570
Robert Sturla <robertsturla@outlook.com>
2024-10-12 19:21:00 +0100

    chore: update script

--- build_files/wallpaper.sh
+++ build_files/wallpaper.sh
@@ -2,10 +2,14 @@
 
 set -ouex pipefail
 
-if test "$BASE_IMAGE_NAME" = "silverblue"; then
+if test "${BASE_IMAGE_NAME}" = "silverblue"; then
     DATE_MONTH=$(date +%B | tr '[:upper:]' '[:lower:]')
 
+    WALLPAPER_URI="/usr/share/backgrounds/bluefin/bluefin-${DATE_MONTH}-dynamic.xml"
     WALLPAPER_GSCHEMA_OVERRIDE_FILE="/usr/share/glib-2.0/schemas/zz0-bluefin-modifications.gschema.override"
-    sed -i "s|picture-uri='file:///usr/share/backgrounds/bluefin/bluefin-.*-dynamic.xml'|picture-uri='file:///usr/share/backgrounds/bluefin/bluefin-${DATE_MONTH}-dynamic.xml'|g" "${WALLPAPER_GSCHEMA_OVERRIDE_FILE}"
-    sed -i "s|picture-uri-dark='file:///usr/share/backgrounds/bluefin/bluefin-.*-dynamic.xml'|picture-uri-dark='file:///usr/share/backgrounds/bluefin/bluefin-${DATE_MONTH}-dynamic.xml'|g" "${WALLPAPER_GSCHEMA_OVERRIDE_FILE}"
+
+    echo "Setting wallpaper URI to ${WALLPAPER_URI}"
+
+    sed -i "s|picture-uri='DO_NOT_CHANGE'|picture-uri='file://${WALLPAPER_URI}'|g" "${WALLPAPER_GSCHEMA_OVERRIDE_FILE}"
+    sed -i "s|picture-uri-dark='DO_NOT_CHANGE'|picture-uri-dark='file://${WALLPAPER_URI}'|g" "${WALLPAPER_GSCHEMA_OVERRIDE_FILE}"
 fi
