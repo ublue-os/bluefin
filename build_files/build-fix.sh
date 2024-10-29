8bcf0febbcad8d2cea956704d0dece64d4549799
Benjamin Sherman <benjamin@holyarmy.org>
2024-10-29 12:31:47 -0500

    chore: fix builds by enabling updates

---
+++ build_files/build-fix.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/bash
+
+set -eoux pipefail
+
+# This script provides fixes to packages known to have caused build skew.
+# It works by force replacing packages on the FROM image with current
+# packages from fedora update repos.
+
+repos=(
+    fedora-updates.repo
+    fedora-updates-archive.repo
+)
+
+for repo in "${repos[@]}"; do
+    if [ $(grep -c "enabled=1" /etc/yum.repos.d/${repo}) -eq 0 ]; then
+        sed -i "0,/enabled=0/{s/enabled=0/enabled=1/}" /etc/yum.repos.d/${repo}
+    fi
+done
+
+rpm-ostree override replace \
+    --experimental \
+    --from repo=updates \
+        elfutils-libelf \
+        elfutils-libs \
+        || true
