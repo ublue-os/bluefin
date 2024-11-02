8ebccc5662502e2a1228b155c83407a5ec544add
Jorge O. Castro <jorge.castro@gmail.com>
2024-11-02 18:44:12 -0400

    fix(framework): turn the framework akmod back on (#1857)
    Co-authored-by: befanyt <172807209+befanyt@users.noreply.github.com>
    Co-authored-by: Benjamin Sherman <benjamin@holyarmy.org>

--- build_files/install-akmods.sh
+++ build_files/install-akmods.sh
@@ -15,8 +15,8 @@ sed -i 's@enabled=0@enabled=1@g' /etc/yum.repos.d/_copr_ublue-os-akmods.repo
 # Everyone
 rpm-ostree install \
     /tmp/akmods/kmods/*xone*.rpm \
-    /tmp/akmods/kmods/*openrazer*.rpm
-    # /tmp/akmods-rpms/kmods/*framework-laptop*.rpm
+    /tmp/akmods/kmods/*openrazer*.rpm \
+    /tmp/akmods/kmods/*framework-laptop*.rpm
 
 # rpmfusion dependent kmods
 rpm-ostree install \
