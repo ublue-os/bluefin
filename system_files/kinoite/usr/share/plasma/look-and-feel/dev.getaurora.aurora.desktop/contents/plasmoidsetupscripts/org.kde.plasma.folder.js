e0cc1fff51182f605b97790921a6fa83f8d5d440
Niklas Haiden <niklas.haiden@gmail.com>
2024-11-05 00:57:50 +0100

    feat(aurora): update theme with new wallpaper and logos

--- system_files/kinoite/usr/share/plasma/look-and-feel/dev.getaurora.aurora.desktop/contents/plasmoidsetupscripts/org.kde.plasma.folder.js
+++ system_files/kinoite/usr/share/plasma/look-and-feel/dev.getaurora.aurora.desktop/contents/plasmoidsetupscripts/org.kde.plasma.folder.js
@@ -1,4 +1,4 @@
 applet.wallpaperPlugin = 'org.kde.image'
 applet.currentConfigGroup = ["Wallpaper", "org.kde.image", "General"]
-applet.writeConfig("Image", "/usr/share/wallpapers/aurora-wallpaper-1.jxl")
+applet.writeConfig("Image", "/usr/share/wallpapers/aurora-wallpaper-1/contents/images/15392x8616.jpg")
 applet.reloadConfig()
