39602d676acaf36485553f9c650a15b6428fdeb2
m2Giles <69128853+m2Giles@users.noreply.github.com>
2024-11-09 14:02:21 -0500

    add back xpadneo

--- build_files/base/03-install-kernel-akmods.sh
+++ build_files/base/03-install-kernel-akmods.sh
@@ -30,6 +30,7 @@ mv /tmp/rpms/* /tmp/akmods/
 sed -i 's@enabled=0@enabled=1@g' /etc/yum.repos.d/_copr_ublue-os-akmods.repo
 rpm-ostree install \
     /tmp/akmods/kmods/*xone*.rpm \
+    /tmp/akmods/kmods/*xpadneo*.rpm \
     /tmp/akmods/kmods/*framework-laptop*.rpm
 
 # RPMFUSION Dependent AKMODS
