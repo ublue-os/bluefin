3313263e447aad57115f4ef47cef2d621a2914d6
m2Giles <69128853+m2Giles@users.noreply.github.com>
2024-11-09 14:16:13 -0500

    add back openrazer

--- build_files/base/03-install-kernel-akmods.sh
+++ build_files/base/03-install-kernel-akmods.sh
@@ -31,6 +31,7 @@ sed -i 's@enabled=0@enabled=1@g' /etc/yum.repos.d/_copr_ublue-os-akmods.repo
 rpm-ostree install \
     /tmp/akmods/kmods/*xone*.rpm \
     /tmp/akmods/kmods/*xpadneo*.rpm \
+    /tmp/akmods/kmods/*openrazer*.rpm \
     /tmp/akmods/kmods/*framework-laptop*.rpm
 
 # RPMFUSION Dependent AKMODS
