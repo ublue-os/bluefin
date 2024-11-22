2788de374ecd2f9b2ab5c5b72fe1d020fed41ebe
Jorge O. Castro <jorge.castro@gmail.com>
2024-11-22 12:23:31 -0700

    fix: pin pkcs11-provider to 0.5-3.fc41 (#1959)

--- build_files/base/18-workarounds.sh
+++ build_files/base/18-workarounds.sh
@@ -6,3 +6,10 @@ set -eoux pipefail
 if [[ -f /usr/bin/ld.bfd ]]; then
     ln -sf /usr/bin/ld.bfd /etc/alternatives/ld && ln -sf /etc/alternatives/ld /usr/bin/ld
 fi
+
+
+## Pins and Overrides
+## Use this section to pin packages in order to avoid regressions
+if [ "$FEDORA_MAJOR_VERSION" -eq "41" ]; then
+    rpm-ostree override replace https://bodhi.fedoraproject.org/updates/FEDORA-2024-dd2e9fb225    
+fi
