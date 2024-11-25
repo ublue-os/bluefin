b6b35f9f332560801d88adbb4b0ba7d3ba5c931d
Benjamin Sherman <benjamin@holyarmy.org>
2024-11-25 11:07:32 -0600

    feat: set mutter experimental kms-modifiers for nvidia (#1978)

--- build_files/base/07-base-image-changes.sh
+++ build_files/base/07-base-image-changes.sh
@@ -63,11 +63,22 @@ elif [[ "${BASE_IMAGE_NAME}" = "silverblue" ]]; then
         sed -i 's@\[Desktop Entry\]@\[Desktop Entry\]\nHidden=true@g' /usr/share/applications/org.gnome.SystemMonitor.desktop
     fi
 
+    # Add Mutter experimental-features
+    MUTTER_EXP_FEATS="'scale-monitor-framebuffer', 'xwayland-native-scaling'"
+    if [[ "${IMAGE_NAME}" =~ nvidia ]]; then
+        MUTTER_EXP_FEATS="'kms-modifiers', ${MUTTER_EXP_FEATS}"
+    fi
+    tee /usr/share/glib-2.0/schemas/zz1-bluefin-modifications-mutter-exp-feats.gschema.override << EOF
+[org.gnome.mutter]
+experimental-features=[${MUTTER_EXP_FEATS}]
+EOF
+
     # GNOME Terminal is replaced with Ptyxis in F41+
+    # Make schema valid on GNOME <47 which do not contain the accent-color key or xwayland-native-scaling mutter feature
     if [[ "${FEDORA_MAJOR_VERSION}" -lt "41" ]]; then
         sed -i 's@\[Desktop Entry\]@\[Desktop Entry\]\nNoDisplay=true@g' /usr/share/applications/org.gnome.Terminal.desktop
         sed -i 's@accent-color="slate"@@g' /usr/share/glib-2.0/schemas/zz0-bluefin-modifications.gschema.override
-        sed -i 's@'", "\''xwayland-native-scaling'\''@@g' /usr/share/glib-2.0/schemas/zz0-bluefin-modifications.gschema.override
+        sed -i 's@'", "\''xwayland-native-scaling'\''@@g' /usr/share/glib-2.0/schemas/zz1-bluefin-modifications-mutter-exp-feats.gschema.override
     fi
 
     # Create symlinks from old to new wallpaper names for backwards compatibility
@@ -84,8 +95,8 @@ elif [[ "${BASE_IMAGE_NAME}" = "silverblue" ]]; then
     mkdir -p /tmp/bluefin-schema-test
     find /usr/share/glib-2.0/schemas/ -type f ! -name "*.gschema.override" -exec cp {} /tmp/bluefin-schema-test/ \;
     cp /usr/share/glib-2.0/schemas/zz0-bluefin-modifications.gschema.override /tmp/bluefin-schema-test/
+    cp /usr/share/glib-2.0/schemas/zz1-bluefin-modifications-mutter-exp-feats.gschema.override /tmp/bluefin-schema-test/
     echo "Running error test for bluefin gschema override. Aborting if failed."
-    # We are omitting "--strict" from the schema validation since GNOME <47 do not contain the accent-color keys.
     # We should ideally refactor this to handle multiple GNOME version schemas better
     glib-compile-schemas --strict /tmp/bluefin-schema-test
     echo "Compiling gschema to include bluefin setting overrides"
