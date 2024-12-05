d09e112772d0792a9d73eee983e6d7abef30203a
Tulip Blossom <tulilirockz@outlook.com>
2024-12-05 13:08:24 -0300

    feat: accent color theming for fastfetch and MOTD

--- system_files/silverblue/etc/profile.d/bluefin-fastfetch.sh
+++ system_files/silverblue/etc/profile.d/bluefin-fastfetch.sh
@@ -2,4 +2,4 @@
 
 BLUEFIN_FETCH_LOGO="$(/usr/bin/find /usr/share/ublue-os/bluefin-logos/symbols/* | /usr/bin/shuf -n 1 )"
 
-alias fastfetch='/usr/bin/fastfetch --logo ${BLUEFIN_FETCH_LOGO}  -c /usr/share/ublue-os/ublue-os.jsonc'
+alias fastfetch='/usr/bin/fastfetch --logo ${BLUEFIN_FETCH_LOGO} --color $(/usr/libexec/ublue-bling-fastfetch) -c /usr/share/ublue-os/ublue-os.jsonc'
