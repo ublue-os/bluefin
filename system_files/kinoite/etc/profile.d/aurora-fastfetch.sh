882d749f857b04282052bb15a35c9feaf0f08ce0
Tulip Blossom <tulilirockz@outlook.com>
2024-12-05 14:10:43 -0300

    feat: accent color theming for fastfetch and MOTD (#2007)

--- system_files/kinoite/etc/profile.d/aurora-fastfetch.sh
+++ system_files/kinoite/etc/profile.d/aurora-fastfetch.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/sh
 
-alias fastfetch="/usr/bin/fastfetch --file /usr/share/ublue-os/aurora-logo.txt --logo-type file --logo-color-1 94 --logo-color-2 95 --logo-color-3 91  --logo-color-4 97 -c /usr/share/ublue-os/ublue-os.jsonc"
+alias fastfetch='/usr/bin/fastfetch --color $(/usr/libexec/ublue-bling-fastfetch) --file /usr/share/ublue-os/aurora-logo.txt --logo-type file --logo-color-1 94 --logo-color-2 95 --logo-color-3 91  --logo-color-4 97 -c /usr/share/ublue-os/ublue-os.jsonc'
--- system_files/silverblue/etc/fish/conf.d/bluefin-fastfetch.fish
+++ system_files/silverblue/etc/fish/conf.d/bluefin-fastfetch.fish
@@ -2,5 +2,5 @@
 #shellcheck disable=all
 function fastfetch
     set BLUEFIN_FETCH_LOGO (/usr/bin/find "/usr/share/ublue-os/bluefin-logos/symbols/" -mindepth 1 | /usr/bin/shuf -n 1)
-    /usr/bin/fastfetch --logo $BLUEFIN_FETCH_LOGO -c "/usr/share/ublue-os/ublue-os.jsonc"
+    /usr/bin/fastfetch --logo $BLUEFIN_FETCH_LOGO --color (/usr/libexec/ublue-bling-fastfetch) -c "/usr/share/ublue-os/ublue-os.jsonc"
 end
