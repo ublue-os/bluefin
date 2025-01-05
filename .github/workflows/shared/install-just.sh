8a378da4fc23c203a07b2a3f63c35ea8fc34fd8c
Adam Fidel <adam@fidel.id>
2025-01-04 19:40:15 -0600

    use loop if JUST_VERSION is garbage

--- .github/workflows/shared/install-just.sh
+++ .github/workflows/shared/install-just.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -eoux pipefail
-JUST_VERSION=$(curl --retry 3 --retry-all-errors -L https://api.github.com/repos/casey/just/releases/latest | jq -r '.tag_name')
-if [[ "${JUST_VERSION}" == "null" ]]; then
-  JUST_VERSION=1.38.0
-fi
+while [[ "${JUST_VERSION:-}" =~ null || -z "${JUST_VERSION:-}" ]]
+do
+    JUST_VERSION=$(curl -L https://api.github.com/repos/casey/just/releases/latest | jq -r '.tag_name')
+done
 curl -sSLO https://github.com/casey/just/releases/download/${JUST_VERSION}/just-${JUST_VERSION}-x86_64-unknown-linux-musl.tar.gz
 tar -zxvf just-${JUST_VERSION}-x86_64-unknown-linux-musl.tar.gz -C /tmp just
 sudo mv /tmp/just /usr/local/bin/just
