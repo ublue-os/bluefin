98d5ffe372d2c3fa44c99b5dde1bca0dda886827
Tulip Blossom <tulilirockz@outlook.com>
2025-01-09 10:15:55 -0300

    chore: remove vestigial bluefin-fastfetch configuration

--- system_files/silverblue/etc/fish/conf.d/bluefin-fastfetch.fish
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/fish
-#shellcheck disable=all
-function fastfetch
-    set BLUEFIN_FETCH_LOGO (/usr/bin/find "/usr/share/ublue-os/bluefin-logos/symbols/" -mindepth 1 | /usr/bin/shuf -n 1)
-    /usr/bin/fastfetch --logo $BLUEFIN_FETCH_LOGO --color (/usr/libexec/ublue-bling-fastfetch) -c "/usr/share/ublue-os/ublue-os.jsonc"
-end
