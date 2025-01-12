9d3a071bb015aba55810889f987a936789d4b082
dreamyukii <49896401+dreamyukii@users.noreply.github.com>
2025-01-13 00:36:59 +0700

    fix: enable bluefin-groups.service

--- build_files/base/17-cleanup.sh
+++ build_files/base/17-cleanup.sh
@@ -15,6 +15,7 @@ systemctl enable ublue-guest-user.service
 systemctl enable brew-setup.service
 systemctl enable brew-upgrade.timer
 systemctl enable brew-update.timer
+systemctl enable bluefin-groups.service
 systemctl --global enable ublue-user-setup.service
 systemctl --global enable podman-auto-update.timer
 systemctl enable check-sb-key.service
