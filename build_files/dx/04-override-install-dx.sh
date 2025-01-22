38ff6a46ad7cb32ab9047057b48c2e154c03db8d
HikariKnight <2557889+HikariKnight@users.noreply.github.com>
2025-01-22 10:04:56 +0100

    feat: add ls-iommu to image It is a tool that lists pci devices and which groups they are in, and it lets you use flags to fine tune what you look for and what info you want.

--- build_files/dx/04-override-install-dx.sh
+++ build_files/dx/04-override-install-dx.sh
@@ -20,4 +20,11 @@ rm -rf /tmp/monaspace-font*
 fc-cache -f /usr/share/fonts/monaspace
 fc-cache --system-only --really-force --verbose
 
+# ls-iommu helper tool for listing devices in iommu groups (PCI Passthrough tool)
+DOWNLOAD_URL=$(curl https://api.github.com/repos/HikariKnight/ls-iommu/releases/latest | jq -r '.assets[] | select(.name| test(".*x86_64.tar.gz$")).browser_download_url')
+curl --retry 3 -Lo /tmp/ls-iommu.tar.gz "$DOWNLOAD_URL"
+tar --no-same-owner --no-same-permissions --no-overwrite-dir -xvzf /tmp/ls-iommu.tar.gz -C /tmp/
+mv /tmp/ls-iommu /usr/bin/
+rm -rf /tmp/ls-iommu*
+
 echo "::endgroup::"
