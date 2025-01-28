42257b225e8b4294639b4748f742596684dd1dff
Robert Sturla <robertsturla@outlook.com>
2025-01-28 16:28:33 +0000

    fix: add the nvidia repo

--- build_files/base/03-install-kernel-akmods.sh
+++ build_files/base/03-install-kernel-akmods.sh
@@ -58,12 +58,15 @@ if [[ "${IMAGE_NAME}" =~ nvidia ]]; then
 
     # Install Nvidia RPMs
     sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/negativo17-fedora-multimedia.repo
+    curl -L https://negativo17.org/repos/fedora-nvidia.repo -o /etc/yum.repos.d/fedora-nvidia.repo
+
     curl -Lo /tmp/nvidia-install.sh https://raw.githubusercontent.com/ublue-os/hwe/main/nvidia-install.sh
     chmod +x /tmp/nvidia-install.sh
     IMAGE_NAME="${BASE_IMAGE_NAME}" RPMFUSION_MIRROR="" /tmp/nvidia-install.sh
     rm -f /usr/share/vulkan/icd.d/nouveau_icd.*.json
     ln -sf libnvidia-ml.so.1 /usr/lib64/libnvidia-ml.so
     sed -i 's@enabled=0@enabled=1@g' /etc/yum.repos.d/negativo17-fedora-multimedia.repo
+    rm /etc/yum.repos.d/fedora-nvidia.repo
 fi
 
 # ZFS for gts/stable
