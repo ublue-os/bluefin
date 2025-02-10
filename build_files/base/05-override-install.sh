0721bca635ee7e72e7782bd901b472df45e9ffda
Alberto García <4411661+GarciaLnk@users.noreply.github.com>
2025-02-10 17:02:53 +0100

    chore: further font cleanup

--- build_files/base/05-override-install.sh
+++ build_files/base/05-override-install.sh
@@ -50,8 +50,4 @@ dnf5 -y swap fedora-logos bluefin-logos
 # Consolidate Just Files
 find /tmp/just -iname '*.just' -exec printf "\n\n" \; -exec cat {} \; >> /usr/share/ublue-os/just/60-custom.just
 
-# Register Fonts
-fc-cache -f /usr/share/fonts/ubuntu
-fc-cache -f /usr/share/fonts/inter
-
 echo "::endgroup::"
