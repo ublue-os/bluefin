6be0a6ef83f870398eaa48185ce8f46c07b6d845
m2 <69128853+m2Giles@users.noreply.github.com>
2025-03-21 22:00:17 -0400

    nvidia-container-toolkit for beta is missing
    Signed-off-by: m2 <69128853+m2Giles@users.noreply.github.com>

--- build_files/base/03-install-kernel-akmods.sh
+++ build_files/base/03-install-kernel-akmods.sh
@@ -77,16 +77,19 @@ if [[ "${IMAGE_NAME}" =~ nvidia ]]; then
     mv /tmp/rpms/* /tmp/akmods-rpms/
 
     # Exclude the Golang Nvidia Container Toolkit in Fedora Repo
-    dnf5 config-manager setopt excludepkgs=golang-github-nvidia-container-toolkit
-
-    # Install Nvidia RPMs
-    curl -Lo /tmp/nvidia-install.sh https://raw.githubusercontent.com/ublue-os/hwe/main/nvidia-install.sh # Change when nvidia-install.sh updates
-    if [[ "${UBLUE_IMAGE_TAG}" == "beta" ]]; then
+    # Exclude for non-beta.... doesn't appear to exist for F42 yet?
+    if [[ "${UBLUE_IMAGE_TAG}" != "beta" ]]; then
+        dnf5 config-manager setopt excludepkgs=golang-github-nvidia-container-toolkit
+    else
+        # Monkey patch right now...
         if ! grep -q negativo17 <(rpm -qi mesa-dri-drivers); then
             dnf5 -y swap --repo=updates-testing \
                 mesa-dri-drivers mesa-dri-drivers
         fi
     fi
+
+    # Install Nvidia RPMs
+    curl -Lo /tmp/nvidia-install.sh https://raw.githubusercontent.com/ublue-os/hwe/main/nvidia-install.sh # Change when nvidia-install.sh updates
     chmod +x /tmp/nvidia-install.sh
     IMAGE_NAME="${BASE_IMAGE_NAME}" RPMFUSION_MIRROR="" /tmp/nvidia-install.sh
     rm -f /usr/share/vulkan/icd.d/nouveau_icd.*.json
