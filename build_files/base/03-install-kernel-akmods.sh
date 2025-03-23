67b801ace2231d2d27fd033befe9f0f2e68eff98
m2 <69128853+m2Giles@users.noreply.github.com>
2025-03-22 23:06:43 -0400

    chore: get beta building.... (#2278)
    Signed-off-by: m2 <69128853+m2Giles@users.noreply.github.com>

--- build_files/base/03-install-kernel-akmods.sh
+++ build_files/base/03-install-kernel-akmods.sh
@@ -4,15 +4,19 @@ echo "::group:: ===$(basename "$0")==="
 
 set -eoux pipefail
 
+# Beta Updates Testing Repo...
+if [[ "${UBLUE_IMAGE_TAG}" == "beta" ]]; then
+    dnf5 config-manager setopt updates-testing.enabled=1
+fi
+
 # Remove Existing Kernel
-for pkg in kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra
-do
+for pkg in kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra; do
     rpm --erase $pkg --nodeps
 done
 
 # Fetch Common AKMODS & Kernel RPMS
 skopeo copy --retry-times 3 docker://ghcr.io/ublue-os/akmods:"${AKMODS_FLAVOR}"-"$(rpm -E %fedora)"-"${KERNEL}" dir:/tmp/akmods
-AKMODS_TARGZ=$(jq -r '.layers[].digest' < /tmp/akmods/manifest.json | cut -d : -f 2)
+AKMODS_TARGZ=$(jq -r '.layers[].digest' </tmp/akmods/manifest.json | cut -d : -f 2)
 tar -xvzf /tmp/akmods/"$AKMODS_TARGZ" -C /tmp/
 mv /tmp/rpms/* /tmp/akmods/
 # NOTE: kernel-rpms should auto-extract into correct location
@@ -32,18 +36,32 @@ dnf5 versionlock add kernel kernel-devel kernel-devel-matched kernel-core kernel
 # Everyone
 # NOTE: we won't use dnf5 copr plugin for ublue-os/akmods until our upstream provides the COPR standard naming
 sed -i 's@enabled=0@enabled=1@g' /etc/yum.repos.d/_copr_ublue-os-akmods.repo
-dnf5 -y install \
-    /tmp/akmods/kmods/*xone*.rpm \
-    /tmp/akmods/kmods/*xpadneo*.rpm \
-    /tmp/akmods/kmods/*openrazer*.rpm \
-    /tmp/akmods/kmods/*framework-laptop*.rpm
+if [[ "${UBLUE_IMAGE_TAG}" == "beta" ]]; then
+    dnf5 -y install /tmp/akmods/kmods/*xone*.rpm || true
+    dnf5 -y install /tmp/akmods/kmods/*xpadneo*.rpm || true
+    dnf5 -y install /tmp/akmods/kmods/*openrazer*.rpm || true
+    dnf5 -y install /tmp/akmods/kmods/*framework-laptop*.rpm || true
+else
+    dnf5 -y install \
+        /tmp/akmods/kmods/*xone*.rpm \
+        /tmp/akmods/kmods/*xpadneo*.rpm \
+        /tmp/akmods/kmods/*openrazer*.rpm \
+        /tmp/akmods/kmods/*framework-laptop*.rpm
+fi
 
 # RPMFUSION Dependent AKMODS
 dnf5 -y install \
     https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-"$(rpm -E %fedora)".noarch.rpm \
     https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-"$(rpm -E %fedora)".noarch.rpm
-dnf5 -y install \
-    v4l2loopback /tmp/akmods/kmods/*v4l2loopback*.rpm
+
+if [[ "${UBLUE_IMAGE_TAG}" == "beta" ]]; then
+    dnf5 -y install \
+        v4l2loopback /tmp/akmods/kmods/*v4l2loopback*.rpm || true
+else
+    dnf5 -y install \
+        v4l2loopback /tmp/akmods/kmods/*v4l2loopback*.rpm
+fi
+
 dnf5 -y remove rpmfusion-free-release rpmfusion-nonfree-release
 
 # Nvidia AKMODS
@@ -54,12 +72,22 @@ if [[ "${IMAGE_NAME}" =~ nvidia ]]; then
     else
         skopeo copy --retry-times 3 docker://ghcr.io/ublue-os/akmods-nvidia:"${AKMODS_FLAVOR}"-"$(rpm -E %fedora)"-"${KERNEL}" dir:/tmp/akmods-rpms
     fi
-    NVIDIA_TARGZ=$(jq -r '.layers[].digest' < /tmp/akmods-rpms/manifest.json | cut -d : -f 2)
+    NVIDIA_TARGZ=$(jq -r '.layers[].digest' </tmp/akmods-rpms/manifest.json | cut -d : -f 2)
     tar -xvzf /tmp/akmods-rpms/"$NVIDIA_TARGZ" -C /tmp/
     mv /tmp/rpms/* /tmp/akmods-rpms/
 
     # Exclude the Golang Nvidia Container Toolkit in Fedora Repo
-    dnf5 config-manager setopt excludepkgs=golang-github-nvidia-container-toolkit
+    # Exclude for non-beta.... doesn't appear to exist for F42 yet?
+    if [[ "${UBLUE_IMAGE_TAG}" != "beta" ]]; then
+        dnf5 config-manager setopt excludepkgs=golang-github-nvidia-container-toolkit
+    else
+        # Monkey patch right now...
+        if ! grep -q negativo17 <(rpm -qi mesa-dri-drivers); then
+            dnf5 -y swap --repo=updates-testing \
+                mesa-dri-drivers mesa-dri-drivers
+        fi
+    fi
+
     # Install Nvidia RPMs
     curl -Lo /tmp/nvidia-install.sh https://raw.githubusercontent.com/ublue-os/hwe/main/nvidia-install.sh # Change when nvidia-install.sh updates
     chmod +x /tmp/nvidia-install.sh
@@ -72,7 +100,7 @@ fi
 if [[ ${AKMODS_FLAVOR} =~ coreos ]]; then
     # Fetch ZFS RPMs
     skopeo copy --retry-times 3 docker://ghcr.io/ublue-os/akmods-zfs:"${AKMODS_FLAVOR}"-"$(rpm -E %fedora)"-"${KERNEL}" dir:/tmp/akmods-zfs
-    ZFS_TARGZ=$(jq -r '.layers[].digest' < /tmp/akmods-zfs/manifest.json | cut -d : -f 2)
+    ZFS_TARGZ=$(jq -r '.layers[].digest' </tmp/akmods-zfs/manifest.json | cut -d : -f 2)
     tar -xvzf /tmp/akmods-zfs/"$ZFS_TARGZ" -C /tmp/
     mv /tmp/rpms/* /tmp/akmods-zfs/
 
@@ -93,7 +121,7 @@ if [[ ${AKMODS_FLAVOR} =~ coreos ]]; then
 
     # Depmod and autoload
     depmod -a -v "${KERNEL}"
-    echo "zfs" > /usr/lib/modules-load.d/zfs.conf
+    echo "zfs" >/usr/lib/modules-load.d/zfs.conf
 fi
 
 echo "::endgroup::"
