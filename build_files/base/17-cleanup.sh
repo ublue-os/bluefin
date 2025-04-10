15c29c35ff4e82cdd9b0a122bde9d387c693b87d
Tulip Blossom <tulilirockz@outlook.com>
2025-04-10 15:31:03 -0300

    chore: switch from ublue-update to uupd as default automatic updater (#2136)
    Signed-off-by: m2 <69128853+m2Giles@users.noreply.github.com>
    Co-authored-by: m2 <69128853+m2Giles@users.noreply.github.com>
    Co-authored-by: Gareth Widlansky <101901964+gerblesh@users.noreply.github.com>
    Co-authored-by: Jorge O. Castro <jorge.castro@gmail.com>

--- build_files/base/17-cleanup.sh
+++ build_files/base/17-cleanup.sh
@@ -9,9 +9,6 @@ systemctl enable rpm-ostree-countme.service
 systemctl enable tailscaled.service
 systemctl enable dconf-update.service
 systemctl --global enable ublue-flatpak-manager.service
-systemctl enable rpm-ostreed-automatic.timer
-systemctl enable flatpak-system-update.timer
-systemctl --global enable flatpak-user-update.timer
 systemctl enable ublue-system-setup.service
 systemctl enable ublue-guest-user.service
 systemctl enable brew-setup.service
@@ -20,6 +17,15 @@ systemctl enable brew-update.timer
 systemctl --global enable ublue-user-setup.service
 systemctl --global enable podman-auto-update.timer
 systemctl enable check-sb-key.service
+
+# Updater
+if systemctl cat -- uupd.timer &> /dev/null; then
+    systemctl enable uupd.timer
+else
+    systemctl enable rpm-ostreed-automatic.timer
+    systemctl enable flatpak-system-update.timer
+    systemctl --global enable flatpak-user-update.timer
+fi
 
 # Hide Desktop Files. Hidden removes mime associations
 for file in fish htop nvtop; do
