fa23c87eaef6677dfb44ac816a0c390923e908dc
m2 <69128853+m2Giles@users.noreply.github.com>
2025-04-12 11:47:19 -0400

    feat: install v4l2loopback from terra (#2339)
    Signed-off-by: m2 <69128853+m2Giles@users.noreply.github.com>

--- build_files/base/02-install-copr-repos.sh
+++ build_files/base/02-install-copr-repos.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/bash
+# shellcheck disable=SC2016
 
 echo "::group:: ===$(basename "$0")==="
 
@@ -12,5 +13,10 @@ dnf5 -y copr enable ublue-os/packages
 
 # Add Nerd Fonts Repo
 dnf5 -y copr enable che/nerd-fonts
+
+# Add Terra
+dnf5 -y install --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/terra$releasever' terra-release
+dnf5 -y install terra-release-extras || true
+dnf5 config-manager setopt "terra*".enabled=0
 
 echo "::endgroup::"
