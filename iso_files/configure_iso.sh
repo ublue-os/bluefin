0b3e1c6cb0e79e4bd56912a66a2e3c856b1c0354
Robert Sturla <robertsturla@outlook.com>
2025-04-12 14:29:53 +0100

    feat(liveiso): add flatpaks, fix readymade install, correct image tag (#2336)

--- iso_files/configure_iso.sh
+++ iso_files/configure_iso.sh
@@ -2,7 +2,7 @@
 
 set -x
 
-dnf install --nogpgcheck --repofrompath 'um,https://repos.fyralabs.com/um$releasever' readymade
+dnf --enablerepo="terra" install -y readymade
 
 tee /etc/readymade.toml <<EOF
 
