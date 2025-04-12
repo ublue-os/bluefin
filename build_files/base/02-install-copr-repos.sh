fba9635edd165cedb51e39a634b5e670aa0b1e70
m2 <69128853+m2Giles@users.noreply.github.com>
2025-04-12 10:45:49 -0400

    install terra-release first
    Signed-off-by: m2 <69128853+m2Giles@users.noreply.github.com>

--- build_files/base/02-install-copr-repos.sh
+++ build_files/base/02-install-copr-repos.sh
@@ -15,7 +15,8 @@ dnf5 -y copr enable ublue-os/packages
 dnf5 -y copr enable che/nerd-fonts
 
 # Add Terra
-dnf5 -y install --nogpgcheck --repofrompath --skip-unavailable 'terra,https://repos.fyralabs.com/terra$releasever' terra-release{,-extras}
+dnf5 -y install --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/terra$releasever' terra-release
+dnf5 -y install terra-release-extras || true
 dnf5 config-manager setopt "terra*".enabled=0
 
 echo "::endgroup::"
