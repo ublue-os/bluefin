601d2e4fe9fd867e4082f061262b5df9c129ad9f
Tulip Blossom <tulilirockz@proton.me>
2025-04-16 02:57:13 -0300

    fix(nvidia-open): correctly identify nvidia-open on image-info.json + fix live ISO nvidia

--- build_files/base/00-image-info.sh
+++ build_files/base/00-image-info.sh
@@ -21,6 +21,9 @@ image_flavor="main"
 if [[ "${IMAGE_NAME}" =~ nvidia ]]; then
   image_flavor="nvidia"
 fi
+if [[ "${IMAGE_NAME}" =~ nvidia-open ]]; then
+  image_flavor="nvidia-open"
+fi
 
 cat >$IMAGE_INFO <<EOF
 {
