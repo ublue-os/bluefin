d4afd39b88ec5003ca2938f879d3fe7f41566fde
Robert Sturla <robertsturla@outlook.com>
2025-04-17 23:41:50 +0100

    chore: install Docker CE from stable repos (#2362)

--- build_files/dx/04-override-install-dx.sh
+++ build_files/dx/04-override-install-dx.sh
@@ -28,10 +28,4 @@ tar --no-same-owner --no-same-permissions --no-overwrite-dir -xvzf /tmp/ls-iommu
 mv /tmp/ls-iommu/ls-iommu /usr/bin/
 rm -rf /tmp/ls-iommu*
 
-# Install Docker Release Candidate - remove this when F42 packages are pushed to the stable repos
-# https://download.docker.com/linux/fedora/42/x86_64/stable/Packages/
-if [[ $FEDORA_MAJOR_VERSION -eq 42 ]]; then
-    dnf install -y --enablerepo=docker-ce-testing docker-buildx-plugin docker-ce docker-ce-cli docker-compose-plugin
-fi
-
 echo "::endgroup::"
