86e0dbec79145118142feb78e4f9d81389e4ef71
Daniel Everett <deverett@gmail.com>
2025-04-18 17:55:16 +1000

    feat: dynamic wallpaper with hemisphere detection (#1594)

---
+++ system_files/shared/usr/share/ublue-os/user-setup.hooks.d/20-dynamic-wallpaper.sh
@@ -0,0 +1,12 @@
+# #!/usr/bin/env bash
+
+source /usr/lib/ublue/setup-services/libsetup.sh
+
+version-script dynamic-wallpaper user 1 || exit 0
+
+set -euo pipefail
+
+# Enable dynamic wallpaper service and timer
+echo "Enabling dynamic wallpaper service and timer"
+systemctl --user enable --now bluefin-dynamic-wallpaper.service
+systemctl --user enable --now bluefin-dynamic-wallpaper.timer
