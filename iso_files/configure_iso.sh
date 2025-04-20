f248912a941b4d62ccb31a035e073abf3b808a01
Jorge O. Castro <jorge.castro@gmail.com>
2025-04-20 15:00:40 +0100

    revert: "chore(liveiso): install Readymade from nightly" (#2372)

--- iso_files/configure_iso.sh
+++ iso_files/configure_iso.sh
@@ -2,7 +2,7 @@
 
 set -x
 
-dnf --enablerepo="terra" install -y readymade-nightly
+dnf --enablerepo="terra" install -y readymade
 
 IMAGE_INFO="$(cat /usr/share/ublue-os/image-info.json)"
 IMAGE_TAG="$(jq -c -r '."image-tag"' <<< $IMAGE_INFO)"
