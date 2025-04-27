6005478df4fb8411d209231f91f3e3e371a9acad
Tulip Blossom <tulilirockz@proton.me>
2025-04-27 18:01:49 -0300

    fix(live-iso): functioning bentos config

--- iso_files/configure_iso.sh
+++ iso_files/configure_iso.sh
@@ -26,6 +26,24 @@ bootc_enforce_sigpolicy = true
 bootc_args = ["--skip-fetch-check"]
 $KARGS
 
+[[bento]]
+title = "Welcome to Bluefin!"
+desc = "Get to know your new operating system"
+link = "https://projectbluefin.io"
+icon = "explore-symbolic"
+
+[[bento]]
+title = "Need help?"
+desc = "Ask questions on our forums!"
+link = "https://universal-blue.discourse.group/"
+icon = "explore-symbolic"
+
+[[bento]]
+title = "Read our documentation"
+desc = "Placeholder text, please jorge change this cuz you write better"
+link = "https://docs.projectbluefin.io"
+icon = "explore-symbolic"
+
 [distro]
 name = "Bluefin"
 
