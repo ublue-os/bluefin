a8aa7d9048fe41e3adf1b56bfb944a40b0da383d
Micah Dutro <51960903+MDutro@users.noreply.github.com>
2025-05-02 13:54:49 -0700

    fix: remove Distrobox from automatic updates (#2458)

--- build_files/base/17-cleanup.sh
+++ build_files/base/17-cleanup.sh
@@ -4,6 +4,12 @@ echo "::group:: ===$(basename "$0")==="
 
 set -eoux pipefail
 
+# Prevent Distrobox containers from being updated via the background service
+if [[ "$(rpm -E %fedora)" -eq "42" ]]; then
+    sed -i 's|uupd|& --disable-module-distrobox|' /usr/lib/systemd/system/uupd.service
+fi
+
+
 # Setup Systemd
 systemctl enable rpm-ostree-countme.service
 systemctl enable tailscaled.service
