42a822125e2aa87d8672f36ff4555257c8ff3066
Tulip Blossom <tulilirockz@proton.me>
2025-05-03 22:30:52 -0300

    chore: dont touch the guest user stuff

--- build_files/base/17-cleanup.sh
+++ build_files/base/17-cleanup.sh
@@ -8,6 +8,7 @@ set -eoux pipefail
 systemctl enable rpm-ostree-countme.service
 systemctl enable tailscaled.service
 systemctl enable dconf-update.service
+systemctl enable ublue-guest-user.service
 systemctl enable brew-setup.service
 systemctl enable brew-upgrade.timer
 systemctl enable brew-update.timer
