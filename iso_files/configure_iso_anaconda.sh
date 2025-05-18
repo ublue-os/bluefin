6681b91d6938be2a19b13793cd2aa46a096916ce
Jorge O. Castro <jorge.castro@gmail.com>
2025-05-18 10:27:04 -0400

    feat(iso): remove a ton of fonts (#2543)

--- iso_files/configure_iso_anaconda.sh
+++ iso_files/configure_iso_anaconda.sh
@@ -11,6 +11,9 @@ IMAGE_REF="${IMAGE_REF##*://}"
 sbkey='https://github.com/ublue-os/akmods/raw/main/certs/public_key.der'
 
 # Configure Live Environment
+## Remove packages from liveCD to save space
+
+dnf remove -y google-noto-fonts-all ublue-brew ublue-motd yaru-theme || true
 
 # Setup dock
 tee /usr/share/glib-2.0/schemas/org.gnome.shell.gschema.override <<EOF
