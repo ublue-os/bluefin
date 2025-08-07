85019dd35c8e9f9efbf351083c0843f81330e26c
Ahmed Adan <ahmed.adan@gmail.com>
2025-08-06 22:54:21 -0400

    chore: change default filesystem from btrfs to xfs

--- iso_files/configure_iso_anaconda.sh
+++ iso_files/configure_iso_anaconda.sh
@@ -85,12 +85,11 @@ efi_dir = fedora
 menu_auto_hide = True
 
 [Storage]
-default_scheme = BTRFS
-btrfs_compression = zstd:1
+file_system_type= xfs
 default_partitioning =
     /     (min 1 GiB, max 70 GiB)
     /home (min 500 MiB, free 50 GiB)
-    /var  (btrfs)
+    /var 
 
 [User Interface]
 custom_stylesheet = /usr/share/anaconda/pixmaps/silverblue/fedora-silverblue.css
