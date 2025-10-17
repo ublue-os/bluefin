ab1dab94733c7ad78bec5ead16b896da46b5420b
Ahmed Adan <ahmed.adan@gmail.com>
2025-10-16 22:11:01 -0400

    fix: filter excluded packages from include list before installation (#3401)

--- build_files/base/04-packages.sh
+++ build_files/base/04-packages.sh
@@ -9,17 +9,22 @@ readarray -t INCLUDED_PACKAGES < <(jq -r "[(.all.include | (select(.all != null)
                     (select(.\"$FEDORA_MAJOR_VERSION\" != null).\"$FEDORA_MAJOR_VERSION\".include | (select(.all != null).all)[])] \
                     | sort | unique[]" /tmp/packages.json)
 
+# build list of all packages requested for exclusion
+readarray -t EXCLUDED_PACKAGES < <(jq -r "[(.all.exclude | (select(.all != null).all)[]), \
+                    (select(.\"$FEDORA_MAJOR_VERSION\" != null).\"$FEDORA_MAJOR_VERSION\".exclude | (select(.all != null).all)[])] \
+                    | sort | unique[]" /tmp/packages.json)
+
+# Filter out excluded packages from the include list
+if [[ "${#EXCLUDED_PACKAGES[@]}" -gt 0 && "${#INCLUDED_PACKAGES[@]}" -gt 0 ]]; then
+    readarray -t INCLUDED_PACKAGES < <(printf '%s\n' "${INCLUDED_PACKAGES[@]}" | grep -vFx -f <(printf '%s\n' "${EXCLUDED_PACKAGES[@]}"))
+fi
+
 # Install Packages
 if [[ "${#INCLUDED_PACKAGES[@]}" -gt 0 ]]; then
     dnf5 -y install "${INCLUDED_PACKAGES[@]}"
 else
     echo "No packages to install."
 fi
-
-# build list of all packages requested for exclusion
-readarray -t EXCLUDED_PACKAGES < <(jq -r "[(.all.exclude | (select(.all != null).all)[]), \
-                    (select(.\"$FEDORA_MAJOR_VERSION\" != null).\"$FEDORA_MAJOR_VERSION\".exclude | (select(.all != null).all)[])] \
-                    | sort | unique[]" /tmp/packages.json)
 
 if [[ "${#EXCLUDED_PACKAGES[@]}" -gt 0 ]]; then
     readarray -t EXCLUDED_PACKAGES < <(rpm -qa --queryformat='%{NAME}\n' "${EXCLUDED_PACKAGES[@]}")
