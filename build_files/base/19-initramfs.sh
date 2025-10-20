b48a9a289efc4b3bb64cbd105c90ae07e758174e
Tulip Blossom <tulilirockz@posteo.com>
2025-10-20 19:20:46 -0300

    fix: kernel suffix on initramfs script
    Signed-off-by: Tulip Blossom <tulilirockz@posteo.com>

--- build_files/base/19-initramfs.sh
+++ build_files/base/19-initramfs.sh
@@ -4,6 +4,7 @@ echo "::group:: ===$(basename "$0")==="
 
 set -oue pipefail
 
+KERNEL_SUFFIX=""
 QUALIFIED_KERNEL="$(rpm -qa | grep -P 'kernel-(|'"$KERNEL_SUFFIX"'-)(\d+\.\d+\.\d+)' | sed -E 's/kernel-(|'"$KERNEL_SUFFIX"'-)//')"
 /usr/bin/dracut --no-hostonly --kver "$QUALIFIED_KERNEL" --reproducible -v --add ostree -f "/lib/modules/$QUALIFIED_KERNEL/initramfs.img"
 chmod 0600 "/lib/modules/$QUALIFIED_KERNEL/initramfs.img"
