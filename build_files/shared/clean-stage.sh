5c689fb4d7904cdd2bdaefa846ff320e5f9312eb
Tulip Blossom <tulilirockz@outlook.com>
2025-10-28 21:11:16 -0300

    cleanup: remove/mask fedora flatpaks service directly on bluefin as well (#3512)
    Signed-off-by: Tulip Blossom <tulilirockz@outlook.com>
    Co-authored-by: Jordan Petridis <16531710+alatiera@users.noreply.github.com>

--- build_files/shared/clean-stage.sh
+++ build_files/shared/clean-stage.sh
@@ -6,6 +6,9 @@ set -eoux pipefail
 
 dnf clean all
 
+systemctl mask flatpak-add-fedora-repos.service
+rm -f /usr/lib/systemd/system/flatpak-add-fedora-repos.service
+
 rm -rf /.gitkeep
 find /var/* -maxdepth 0 -type d \! -name cache -exec rm -fr {} \;
 find /var/cache/* -maxdepth 0 -type d \! -name libdnf5 \! -name rpm-ostree -exec rm -fr {} \;
