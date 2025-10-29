343038e3257c9771dc4d1fe27ebd2bffaa1df806
Tulip Blossom <tulilirockz+git@outlook.com>
2025-10-28 21:10:54 -0300

    Update build_files/shared/clean-stage.sh
    Co-authored-by: Jordan Petridis <16531710+alatiera@users.noreply.github.com>

--- build_files/shared/clean-stage.sh
+++ build_files/shared/clean-stage.sh
@@ -7,7 +7,7 @@ set -eoux pipefail
 dnf clean all
 
 systemctl mask flatpak-add-fedora-repos.service
-rm -rf /usr/lib/systemd/system/flatpak-add-fedora-repos.service
+rm -f /usr/lib/systemd/system/flatpak-add-fedora-repos.service
 
 rm -rf /.gitkeep
 find /var/* -maxdepth 0 -type d \! -name cache -exec rm -fr {} \;
