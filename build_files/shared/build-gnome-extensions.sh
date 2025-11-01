7e5105d6d9f9b8b3f6aefed9165a09467a7b0c3a
Tulip Blossom <tulilirockz@outlook.com>
2025-11-01 03:18:49 -0300

    fix: gschemas broken for whatever reason
    Signed-off-by: Tulip Blossom <tulilirockz@outlook.com>

--- build_files/shared/build-gnome-extensions.sh
+++ build_files/shared/build-gnome-extensions.sh
@@ -10,35 +10,37 @@ dnf5 -y install glib2-devel meson sassc cmake dbus-devel
 # Build Extensions
 
 # AppIndicator Support
-glib-compile-schemas /usr/share/gnome-shell/extensions/appindicatorsupport@rgcjonas.gmail.com/schemas
+glib-compile-schemas --strict /usr/share/gnome-shell/extensions/appindicatorsupport@rgcjonas.gmail.com/schemas
 
-# Logo Menu
-glib-compile-schemas /usr/share/gnome-shell/extensions/logomenu@aryan_k/schemas
+# Blur My Shell
+make -C /usr/share/gnome-shell/extensions/blur-my-shell@aunetx
+unzip -o /usr/share/gnome-shell/extensions/blur-my-shell@aunetx/build/blur-my-shell@aunetx.shell-extension.zip -d /usr/share/gnome-shell/extensions/blur-my-shell@aunetx
+glib-compile-schemas --strict /usr/share/gnome-shell/extensions/blur-my-shell@aunetx/schemas
+rm -rf /usr/share/gnome-shell/extensions/blur-my-shell@aunetx/build
 
 # Caffeine
 # The Caffeine extension is built/packaged into a temporary subdirectory (tmp/caffeine/caffeine@patapon.info).
 # Unlike other extensions, it must be moved to the standard extensions directory so GNOME Shell can detect it.
 mv /usr/share/gnome-shell/extensions/tmp/caffeine/caffeine@patapon.info /usr/share/gnome-shell/extensions/caffeine@patapon.info
-glib-compile-schemas /usr/share/gnome-shell/extensions/caffeine@patapon.info/schemas
-
-# Blur My Shell
-make -C /usr/share/gnome-shell/extensions/blur-my-shell@aunetx
-unzip -o /usr/share/gnome-shell/extensions/blur-my-shell@aunetx/build/blur-my-shell@aunetx.shell-extension.zip -d /usr/share/gnome-shell/extensions/blur-my-shell@aunetx
-glib-compile-schemas /usr/share/gnome-shell/extensions/blur-my-shell@aunetx/schemas
-rm -rf /usr/share/gnome-shell/extensions/blur-my-shell@aunetx/build
+glib-compile-schemas --strict /usr/share/gnome-shell/extensions/caffeine@patapon.info/schemas
 
 # Dash to Dock
 make -C /usr/share/gnome-shell/extensions/dash-to-dock@micxgx.gmail.com
-glib-compile-schemas /usr/share/gnome-shell/extensions/dash-to-dock@micxgx.gmail.com/schemas
+glib-compile-schemas --strict /usr/share/gnome-shell/extensions/dash-to-dock@micxgx.gmail.com/schemas
 
-# GSConnect
-meson setup --prefix=/usr /usr/share/gnome-shell/extensions/gsconnect@andyholmes.github.io /usr/share/gnome-shell/extensions/gsconnect@andyholmes.github.io/_build
-meson install -C /usr/share/gnome-shell/extensions/gsconnect@andyholmes.github.io/_build --skip-subprojects
+# GSConnect (commented out until G49 support)
+# meson setup --prefix=/usr /usr/share/gnome-shell/extensions/gsconnect@andyholmes.github.io /usr/share/gnome-shell/extensions/gsconnect@andyholmes.github.io/_build
+# meson install -C /usr/share/gnome-shell/extensions/gsconnect@andyholmes.github.io/_build --skip-subprojects
 # GSConnect installs schemas to /usr/share/glib-2.0/schemas and meson compiles them automatically
 
+# Logo Menu
+glib-compile-schemas --strict /usr/share/gnome-shell/extensions/logomenu@aryan_k/schemas
+
 # Search Light
-make build -C /usr/share/gnome-shell/extensions/search-light@icedman.github.com
-glib-compile-schemas /usr/share/gnome-shell/extensions/search-light@icedman.github.com/schemas
+glib-compile-schemas --strict /usr/share/gnome-shell/extensions/search-light@icedman.github.com/schemas
+
+rm /usr/share/glib-2.0/schemas/gschemas.compiled
+glib-compile-schemas --strict /usr/share/glib-2.0/schemas
 
 # Cleanup
 dnf5 -y remove glib2-devel meson sassc cmake dbus-devel
