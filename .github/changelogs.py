858fe81bdcbbc63b3c18e5c55a66c3504c1f8655
befanyt <172807209+befanyt@users.noreply.github.com>
2025-11-11 16:55:25 +0100

    fix(changelogs): switch to nvidia-open and drop hwe

--- .github/changelogs.py
+++ .github/changelogs.py
@@ -11,12 +11,12 @@ REGISTRY = "docker://ghcr.io/ublue-os/"
 IMAGE_MATRIX_LATEST = {
     "experience": ["base", "dx"],
     "de": ["gnome"],
-    "image_flavor": ["main", "nvidia", "hwe", "hwe-nvidia"],
+    "image_flavor": ["main", "nvidia-open"],
 }
 IMAGE_MATRIX = {
     "experience": ["base", "dx"],
     "de": ["gnome"],
-    "image_flavor": ["main", "nvidia"],
+    "image_flavor": ["main", "nvidia-open"],
 }
 
 RETRIES = 3
