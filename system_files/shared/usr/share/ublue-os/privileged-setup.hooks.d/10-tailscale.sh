5a98107073ed0abf9af12ac8c1632716e37c87e7
renner <renner0@posteo.de>
2025-12-16 22:57:16 +0100

    chore: remove hooks

--- system_files/shared/usr/share/ublue-os/privileged-setup.hooks.d/10-tailscale.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-
-source /usr/lib/ublue/setup-services/libsetup.sh
-
-version-script tailscale privileged 1 || exit 0
-
-set -xeuo pipefail
-
-tailscale set --operator="$(getent passwd "$PKEXEC_UID" | cut -d: -f1)"
--- system_files/shared/usr/share/ublue-os/user-setup.hooks.d/99-privileged.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-echo "Running all privileged units"
-
-pkexec /usr/libexec/ublue-privileged-setup
