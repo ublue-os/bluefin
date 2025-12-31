a3693c5ba2d5d2c0ec7df9925bc29da1e43e83f5
renner <80410025+renner0e@users.noreply.github.com>
2025-12-31 03:14:19 +0100

    feat: move logos to common (#3851)

--- build_files/base/04-packages.sh
+++ build_files/base/04-packages.sh
@@ -120,7 +120,6 @@ copr_install_isolated "che/nerd-fonts" "nerd-fonts"
 
 # From ublue-os/packages
 copr_install_isolated "ublue-os/packages" \
-    "bluefin-cli-logos" \
     "bluefin-schemas" \
     "ublue-bling" \
     "ublue-fastfetch" \
@@ -191,14 +190,6 @@ if [[ "$(rpm -E %fedora)" -ge "42" ]]; then
   dnf -y --repo=copr:copr.fedorainfracloud.org:ublue-os:flatpak-test swap flatpak-session-helper flatpak-session-helper
   dnf -y --repo=copr:copr.fedorainfracloud.org:ublue-os:flatpak-test install flatpak-debuginfo flatpak-libs-debuginfo flatpak-session-helper-debuginfo
 fi
-
-# Swap/install bluefin branding packages from ublue-os/packages COPR using isolated enablement
-dnf -y swap \
-    --repo=copr:copr.fedorainfracloud.org:ublue-os:packages \
-    fedora-logos bluefin-logos
-dnf -y install \
-    --repo=copr:copr.fedorainfracloud.org:ublue-os:packages \
-    bluefin-plymouth
 
 ## Pins and Overrides
 ## Use this section to pin packages in order to avoid regressions
