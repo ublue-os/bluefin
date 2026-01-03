75befa2621934fa5a81c8902e62e7166c78251a5
renner <renner0@posteo.de>
2026-01-03 02:41:33 +0100

    chore: remove ublue-os-update services

--- build_files/base/17-cleanup.sh
+++ build_files/base/17-cleanup.sh
@@ -29,10 +29,6 @@ fi
 # Updater
 systemctl enable uupd.timer
 
-# Disable the old update timer
-systemctl disable rpm-ostreed-automatic.timer
-systemctl disable flatpak-system-update.timer
-
 # Hide Desktop Files. Hidden removes mime associations
 for file in fish htop nvtop; do
     if [[ -f "/usr/share/applications/$file.desktop" ]]; then
