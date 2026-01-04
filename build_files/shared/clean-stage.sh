033f6cf8347f623d58278d75f2250a8a83c3b39c
renner <80410025+renner0e@users.noreply.github.com>
2026-01-04 16:06:12 +0100

    fix: cleanup /{boot,tmp} (#3922)

--- build_files/shared/clean-stage.sh
+++ build_files/shared/clean-stage.sh
@@ -10,7 +10,9 @@ systemctl mask flatpak-add-fedora-repos.service
 rm -f /usr/lib/systemd/system/flatpak-add-fedora-repos.service
 
 rm -rf /.gitkeep
+find /boot/* -maxdepth 0 -type d \! -name cache -exec rm -fr {} \;
 find /var/* -maxdepth 0 -type d \! -name cache -exec rm -fr {} \;
 find /var/cache/* -maxdepth 0 -type d \! -name libdnf5 \! -name rpm-ostree -exec rm -fr {} \;
+rm -rf /tmp && mkdir -p /tmp
 
 echo "::endgroup::"
