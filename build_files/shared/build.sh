ab19ab3b14d55a22c00f0e473236422896baead5
Tulip Blossom <tulilirockz@outlook.com>
2026-01-20 16:23:59 -0300

    fix: do not remove fedora-logos (#4072)
    Signed-off-by: Tulip Blossom <tulilirockz@outlook.com>
    Co-authored-by: renner <renner0@posteo.de>
    Co-authored-by: renner <80410025+renner0e@users.noreply.github.com>

--- build_files/shared/build.sh
+++ build_files/shared/build.sh
@@ -7,8 +7,10 @@ echo "::group:: Copy Files"
 # We need to remove this package here because lots of files we add from `projectbluefin/common` override the rpm files and they also go away when you do `dnf remove`
 dnf remove -y ublue-os-luks ublue-os-just ublue-os-udev-rules ublue-os-signing ublue-os-update-services
 
-# Conflicts with a ton of packages, has to be removed before we copy all the files as well
-rpm --erase --nodeps fedora-logos
+# Keep *-logos in RPM DB for downstream package installations
+# We are not allowed to ship an empty fedora-logos package
+dnf -y swap fedora-logos generic-logos
+rpm --erase --nodeps --nodb generic-logos
 
 # Copy Files to Container
 rsync -rvK /ctx/system_files/shared/ /
